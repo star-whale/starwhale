ccefb56699c0a7f175981724cb832dc4905a3ec4
goldenxinxing <15031259256@163.com>
2022-03-16 19:08:17 +0800

    feat:add sdk module

---
+++ agent/src/main/java/ai/starwhale/mlops/agent/StarWhaleAgentApplication.java
@@ -0,0 +1,12 @@
+package ai.starwhale.mlops.agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarWhaleAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarWhaleAgentApplication.class, args);
+    }
+}
