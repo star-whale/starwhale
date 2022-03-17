7a01fc3553f4cd7852a327c434bc26fa885ca616
DreamlandLiu <205407@qq.com>
2022-03-17 23:40:34 +0800

    Add params object
    * PageParams and BaseParams

---
+++ controller/src/main/java/ai/starwhale/mlops/common/BaseParams.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright 2022.1-2022
+ * StarWhale.ai All right reserved. This software is the confidential and proprietary information of
+ * StarWhale.ai ("Confidential Information"). You shall not disclose such Confidential Information and shall use it only
+ * in accordance with the terms of the license agreement you entered into with StarWhale.ai.
+ */
+
+package ai.starwhale.mlops.common;
+
+import java.io.Serializable;
+import lombok.Data;
+
+@Data
+public class BaseParams implements Serializable {
+
+}
---
+++ controller/src/main/java/ai/starwhale/mlops/common/PageParams.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2022.1-2022
+ * StarWhale.ai All right reserved. This software is the confidential and proprietary information of
+ * StarWhale.ai ("Confidential Information"). You shall not disclose such Confidential Information and shall use it only
+ * in accordance with the terms of the license agreement you entered into with StarWhale.ai.
+ */
+
+package ai.starwhale.mlops.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+public class PageParams extends BaseParams {
+
+    private int pageNum;
+
+    private int pageSize;
+
+}
