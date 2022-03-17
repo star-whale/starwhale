c003ad6769d803be867e8f6496d3157ec21c0f96
tianwei <liutianweidlut@gmail.com>
2022-03-17 22:26:39 +0800

    add fs ensure_xxx utils

---
+++ client/starwhale/utils/fs.py
@@ -0,0 +1,51 @@
+import os
+import errno
+from pathlib import Path
+
+
+def ensure_file(path: str, content: str, mode: int = 0o644) -> None:
+    p = Path(path)
+    try:
+        _saved = p.open().read()
+    except IOError as e:
+        if e.errno == errno.ENONET:
+            # no such file or directory
+            _saved = ""
+        else:
+            raise
+    if _saved != content:
+        # TODO: add timestamp for tmp file
+        _tmp_f = p.parent / f".{p.name}.tmp"
+        _tmp_f.write_text(content)
+        # TODO: check whether rename atomic
+        _tmp_f.rename(path)
+
+    os.chmod(path, mode)
+
+
+def ensure_dir(path: str, mode: int=0o755, recursion: bool=False) ->None:
+    p = Path(path)
+    if p.exists() and p.is_dir():
+        return
+
+    try:
+        if recursion:
+            os.makedirs(path, mode)
+        else:
+            os.mkdir(path, mode)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            #TODO: add more hunmanable exception log
+            raise
+
+
+def ensure_link(src: str, dest: str) -> None:
+    dirname = os.path.dirname(dest)
+    if dirname and not os.path.exists(dirname):
+        os.makedirs(dirname)
+
+    if os.path.lexists(dest):
+        if os.path.islink(dest) and os.readlink(dest) == src:
+            return
+        os.unlink(dest)
+    os.symlink(src, dest)
