d75f005a58772a6d0fc7df9b6e889d6d7cb7a4d7
tianwei <liutianweidlut@gmail.com>
2022-03-21 21:02:38 +0800

    add swmp model info cmd

--- client/starwhale/swmp/store.py
+++ client/starwhale/swmp/store.py
@@ -6,14 +6,18 @@ from loguru import logger
 from rich.table import Table
 from rich.console import Console
 from rich import box
+from rich.panel import Panel
+from rich.pretty import Pretty
 from fs import open_fs
 from fs.tarfs import TarFS
 
 from starwhale.utils.config import load_swcli_config
-from starwhale.consts import DEFAULT_MANIFEST_NAME
+from starwhale.consts import DEFAULT_MANIFEST_NAME, DEFAULT_MODEL_YAML_NAME
 
 SwmpMeta = namedtuple("SwmpMeta", ["model", "version", "tag", "environment", "created"])
 
+
+LATEST_TAG = "latest"
 
 class ModelPackageLocalStore(object):
 
@@ -35,8 +39,9 @@ class ModelPackageLocalStore(object):
     def list(self, filter=None) -> None:
         #TODO: add filter for list
         #TODO: add expand option for list
+        #TODO: workdir list
 
-        table = Table(title="List swmp in local storage", caption=f"storage @ {self.pkgdir}",
+        table = Table(title="List swmp in local storage", caption=f"@{self.pkgdir}",
                       box=box.SIMPLE)
         table.add_column("Model", justify="right", style="cyan" ,no_wrap=False)
         table.add_column("Version", style="magenta")
@@ -57,29 +62,52 @@ class ModelPackageLocalStore(object):
                 continue
 
             for _fname in pkg_fs.opendir(mdir.name).listdir("."):
-                if _fname != "latest" and not _fname.endswith(".swmp"):
+                if _fname != LATEST_TAG and not _fname.endswith(".swmp"):
                     continue
 
                 _path = self.pkgdir / mdir.name / _fname
                 _manifest = self._load_swmp_manifest(str(_path.resolve()))
-                _tag = _fname if _fname == "latest" else ""
+                _tag = _fname if _fname == LATEST_TAG else ""
 
                 yield SwmpMeta(model=mdir.name, version=_manifest["version"], tag=_tag,
                                environment=_manifest["dep"]["env"], created=_manifest["created_at"])
 
     def _load_swmp_manifest(self, fpath) -> dict:
         with TarFS(fpath) as tar:
-
             return yaml.safe_load(tar.open(DEFAULT_MANIFEST_NAME))
 
-    def push(self, swmp) -> None:
+    def push(self, swmp: str) -> None:
         pass
 
-    def pull(self, swmp) -> None:
+    def pull(self, swmp: str) -> None:
         pass
 
-    def info(self, swmp) -> None:
-        pass
+    def info(self, swmp: str) -> None:
+        if ":" not in swmp:
+            _name, _version = swmp, LATEST_TAG
+        else:
+            _name, _version = swmp.split(":", 1)
+
+        _manifest = self.get_swmp_info(_name, _version)
+        _config_panel = Panel(Pretty(_manifest, expand_all=True), title="inspect _manifest.yaml / model.yaml info")
+        Console().print(_config_panel)
+
+        #TODO: add workdir tree
+
+    def get_swmp_info(self, _name: str, _version: str) -> dict:
+        _workdir = self.workdir / _name / _version
+        _swmp_path = self.pkgdir / _name / (_version if _version == LATEST_TAG else f"{_version}.swmp")
+        if _workdir.exists():
+            _manifest = yaml.safe_load((_workdir / DEFAULT_MANIFEST_NAME).open())
+            _model = yaml.safe_load((_workdir / DEFAULT_MODEL_YAML_NAME).open())
+        else:
+            with TarFS(str(_swmp_path.resolve())) as tar:
+                _manifest = yaml.safe_load(tar.open(DEFAULT_MANIFEST_NAME))
+                _model = yaml.safe_load(tar.open(DEFAULT_MODEL_YAML_NAME))
+        _manifest.update(_model)
+        _manifest["workdir"] = str(_workdir.resolve())
+        _manifest["pkg"] = str(_swmp_path.resolve())
+        return _manifest
 
     def gc(self, dry_run=False) -> None:
         pass
