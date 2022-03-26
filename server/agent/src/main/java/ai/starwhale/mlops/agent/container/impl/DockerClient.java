ddad453c204d5c1858bfeb56d37e6fa2a1f67e44
goldenxinxing <15031259256@163.com>
2022-03-26 10:52:27 +0800

    feat:add docker client config

--- server/agent/src/main/java/ai/starwhale/mlops/agent/container/impl/DockerClient.java
+++ server/agent/src/main/java/ai/starwhale/mlops/agent/container/impl/DockerClient.java
@@ -3,12 +3,21 @@ package ai.starwhale.mlops.agent.container.impl;
 import ai.starwhale.mlops.agent.container.ContainerClient;
 
 import ai.starwhale.mlops.agent.container.ImageConfig;
+import com.github.dockerjava.transport.DockerHttpClient;
 import java.util.Optional;
+import org.springframework.stereotype.Service;
 
 /**
  * Docker client
  */
+@Service
 public class DockerClient implements ContainerClient {
+
+    private final DockerHttpClient client;
+
+    public DockerClient(DockerHttpClient client) {
+        this.client = client;
+    }
 
     @Override
     public Optional<String> startContainer(String imageId, ImageConfig imageConfig) {
