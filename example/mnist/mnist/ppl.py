bbb6da406fa6058261a297e521625d19b90a02a2
tianwei <liutianweidlut@gmail.com>
2022-04-01 10:08:38 +0800

    refactor mnist example with starwhale ppl api

--- example/mnist/mnist/ppl.py
+++ example/mnist/mnist/ppl.py
@@ -1,48 +1,62 @@
 from pathlib import Path
 import io
+import typing as t
 
 import torch
 from torchvision import transforms
 from PIL import Image
 
+from starwhale.api.model import PipelineHandler
+
 from model import Net
 
 ROOTDIR = Path(__file__).parent.parent
+ONE_IMAGE_SIZE = 28 * 28
 
 
-class MNISTInference(object):
+class MNISTInference(PipelineHandler):
 
     def __init__(self, device="cpu") -> None:
-        self.model = Net()
+        super().__init__()
         self.device = torch.device(device)
+        self.model = self._load_model(self.device)
 
-    def _pre(self, input):
-        image = Image.open(io.BytesIO(input))
-        image = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.1307,), (0.3081,))
-        ])(image)
-        return torch.stack([image]).to(self.device)
-
-    def _post(self, input):
-        return input.argmax(1).flatten().tolist()[0]
-
-    def inference(self, data):
-        data = self._pre(data)
+    def handle(self, data, batch_size, **kw):
+        data = self._pre(data, batch_size)
         output = self.model(data)
         return self._post(output)
 
-    def load(self):
-        self.model = Net().to(self.device)
-        self.model.load_state_dict(torch.load(str(ROOTDIR / "models/mnist_cnn.pt")))
-        self.model.eval()
+    def _pre(self, input: bytes, batch_size: int):
+        images = []
+        for i in range(0, batch_size):
+            #TODO: tune for batch transforms
+            start = i * ONE_IMAGE_SIZE
+            image = Image.open(io.BytesIO(input[start:(start + ONE_IMAGE_SIZE)]))
+            image = transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Normalize((0.1307,), (0.3081,))
+            ])(image)
+            images.append(image)
+        return torch.stack(images).to(self.device)
+
+    def _post(self, input):
+        return input.argmax(1).flatten().tolist()
+
+    def _load_model(self, device):
+        model = Net().to(device)
+        model.load_state_dict(torch.load(str(ROOTDIR / "models/mnist_cnn.pt")))
+        model.eval()
+        return model
 
 
-if __name__ == "__main__":
+def local_smoketest():
     mnist = MNISTInference()
-    mnist.load()
 
     tdir = ROOTDIR / "data/test"
     for f in tdir.iterdir():
-        output = mnist.inference(f.open("rb").read())
+        output = mnist.handle(f.open("rb").read(), 1)
         print(f"{f.name} -> {output}")
+
+
+if __name__ == "__main__":
+    local_smoketest()
