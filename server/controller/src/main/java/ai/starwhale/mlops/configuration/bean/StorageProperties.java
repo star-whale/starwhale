e776c476ba7293b14817c44d18ce140ed4992dbb
anda-ren <renyanda@starwhale.ai>
2022-04-07 17:59:25 +0800

    fix review issue

--- server/controller/src/main/java/ai/starwhale/mlops/configuration/bean/StorageProperties.java
+++ server/controller/src/main/java/ai/starwhale/mlops/configuration/bean/StorageProperties.java
@@ -6,7 +6,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Data
-@Component
 @ConfigurationProperties(prefix = "storage")
 public class StorageProperties {
 
