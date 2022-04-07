47363234193f4a8a0fa33542a090472ae853eeb3
tianwei <liutianweidlut@gmail.com>
2022-04-07 16:23:17 +0800

    update setup requirements

--- client/setup.py
+++ client/setup.py
@@ -1,12 +1,28 @@
 from setuptools import setup, find_packages
 
-install_requires = open("requirements.txt").readlines()
+install_requires = [
+    'click>=8.0.4',
+    'importlib-metadata==4.8.2',
+    'attrs==21.4.0',
+    'pyyaml==6.0',
+    'cattrs==1.7.1',
+    'requests>=2.27.0',
+    'loguru==0.6.0',
+    'conda-pack==0.6.0',
+    'virtualenv>=13.0.0',
+    'fs>=2.4.0',
+    'typing-extensions>=4.1.1',
+    'commonmark>=0.9.1',
+    'rich==12.0.0',
+    'jsonlines==3.0.0',
+    'boto3==1.21.0',
+]
 
 
 setup(name='starwhale',
       author='Starwhale Team',
       author_email="developer@starwhale.ai",
-      version="0.1.0",
+      version="0.1.0.dev0",
       description='MLOps Platform',
       keywords="MLOps AI",
       url='https://github.com/star-whale/starwhale',
@@ -24,6 +40,5 @@ setup(name='starwhale',
       python_requires = ">=3.7.0",
       scripts=[
       ],
-      package_data={
-      },
+      package_data={},
 )
