85a4e493844cc2c6ffd55e7263063d45f2a85712
tianwei <liutianweidlut@gmail.com>
2022-04-11 22:51:25 +0800

    make  portable venv

--- client/starwhale/utils/venv.py
+++ client/starwhale/utils/venv.py
@@ -50,7 +50,10 @@ def venv_activate(venvdir: t.Union[str, Path]) -> None:
 
 def venv_setup(venvdir: t.Union[str, Path]) -> None:
     #TODO: define starwhale virtualenv.py
-    check_call(['python3', '-m', 'venv', str(venvdir)])
+    #TODO: use more elegant method to make venv portable
+    check_call(f"python3 -m venv --copies {venvdir}", shell=True)
+    check_call(f"sed -i '1s/.*/#!\/usr\/bin\/env python3/' {venvdir}/bin/pip*", shell=True)
+    check_call(f"sed -i '/^export VIRTUAL_ENV/d;/^VIRTUAL_ENV=/d' {venvdir}/bin/activate", shell=True)
 
 
 def pip_freeze(path: t.Union[str, Path]):
@@ -81,7 +84,11 @@ def conda_activate_render(env: t.Union[str, Path], path: Path) -> None:
 
 
 def venv_activate_render(venvdir: t.Union[str, Path], path: Path) -> None:
-    content = f"echo 'source {venvdir}/bin/activate'"
+    content = f"""
+VIRTUAL_ENV={venvdir}
+export VIRTUAL_ENV
+echo 'source {venvdir}/bin/activate'"
+"""
     _render_sw_activate(content, path)
 
 
