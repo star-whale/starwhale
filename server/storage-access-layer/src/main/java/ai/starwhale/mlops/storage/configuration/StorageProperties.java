ff2ad40bf6546cb64403c75cffba3ecde34d89b6
gaoxinxing <15931259256@163.com>
2022-04-12 13:32:57 +0800

    feat:make storage access sdk as a starter

--- server/storage-access-layer/src/main/java/ai/starwhale/mlops/storage/configuration/StorageProperties.java
+++ server/storage-access-layer/src/main/java/ai/starwhale/mlops/storage/configuration/StorageProperties.java
@@ -1,11 +1,11 @@
-package ai.starwhale.mlops.configuration.bean;
+package ai.starwhale.mlops.storage.configuration;
 
 import ai.starwhale.mlops.storage.s3.S3Config;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @Data
-@ConfigurationProperties(prefix = "storage")
+@ConfigurationProperties(prefix = "sw.storage")
 public class StorageProperties {
 
     String pathPrefix;
