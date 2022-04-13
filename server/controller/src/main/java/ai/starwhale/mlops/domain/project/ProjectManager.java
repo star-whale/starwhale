867044b8bd1bd629811b78c5d2441490ab40b240
dreamlandliu <205407@qq.com>
2022-04-13 13:41:01 +0800

    Project Manager (#79)
    * RestAPI for Evaluation Result
    
    * Default project for user
    
    * Project Manager
    
    * fix bug: listProjectsByOwner
    
    * Project Manager

---
+++ server/controller/src/main/java/ai/starwhale/mlops/domain/project/ProjectManager.java
@@ -0,0 +1,27 @@
+package ai.starwhale.mlops.domain.project;
+
+import java.util.List;
+import javax.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class ProjectManager {
+
+    @Resource
+    private ProjectMapper projectMapper;
+
+    public ProjectEntity findDefaultProject(Long userId) {
+        ProjectEntity defaultProject = projectMapper.findDefaultProject(userId);
+        if(defaultProject == null) {
+            List<ProjectEntity> entities = projectMapper.listProjectsByOwner(userId);
+            if(entities.isEmpty()) {
+                log.error("Can not find default project by user, id = {}", userId);
+                return null;
+            }
+            defaultProject = entities.get(0);
+        }
+        return defaultProject;
+    }
+}
