03e18b726190ac4ab3200239ad2f180ff2fac8c5
waynelwz <wytheme.go@gmail.com>
2022-04-14 23:51:05 +0800

    [WIP]feat: indicator of IMBCConfusionMetrics

--- console/src/pages/Job/JobResult.tsx
+++ console/src/pages/Job/JobResult.tsx
@@ -4,36 +4,67 @@ import { Spinner } from 'baseui/spinner'
 import React from 'react'
 import Plot from 'react-plotly.js'
 
-const data = {
+type IDataType = 'MCConfusionMetrics' | 'CohenKappa' | 'MBCConfusionMetrics'
+interface IMCConfusionMetrics {
+    label: string
+    prediction: string
+    name: string
+    value: number
+}
+
+interface IMBCConfusionMetrics {
+    [s: string]: {
+        tp: number
+        tn: number
+        fp: number
+        fn: number
+        accuracy: number
+        precision: number
+        recall: number
+    }
+}
+
+interface IIndicator {
+    name: IDataType
+    value: IMBCConfusionMetrics
+}
+
+interface IDataProps {
+    evaluationType: string
+    indicators: Array<IIndicator>
+    // IMCConfusionMetrics | IMBCConfusionMetrics | ICohenKappa
+}
+
+const data: IDataProps = {
     evaluationType: 'MCResultCollector',
     indicators: [
-        {
-            name: 'MCConfusionMetrics',
-            value: [
-                {
-                    label: 'A',
-                    prediction: 'A',
-                    name: 'A-A',
-                    value: 3,
-                },
-                {
-                    label: 'A',
-                    prediction: 'B',
-                    name: 'A-B',
-                    value: 2,
-                },
-                {
-                    label: 'A',
-                    prediction: 'C',
-                    name: 'A-C',
-                    value: 0,
-                },
-            ],
-        },
-        {
-            name: 'CohenKappa',
-            value: 2.34,
-        },
+        // {
+        //     name: 'MCConfusionMetrics',
+        //     value: [
+        //         {
+        //             label: 'A',
+        //             prediction: 'A',
+        //             name: 'A-A',
+        //             value: 3,
+        //         },
+        //         {
+        //             label: 'A',
+        //             prediction: 'B',
+        //             name: 'A-B',
+        //             value: 2,
+        //         },
+        //         {
+        //             label: 'A',
+        //             prediction: 'C',
+        //             name: 'A-C',
+        //             value: 0,
+        //         },
+        //     ],
+        // },
+        // {
+        //     name: 'CohenKappa',
+        //     value: 2.34,
+        // },
         {
             name: 'MBCConfusionMetrics',
             value: {
@@ -88,6 +119,9 @@ export default function JobResult() {
             title: 'A Graph',
         },
     }
+    const indicator: IIndicator | undefined = data.indicators.find((v) => v.name === 'MBCConfusionMetrics')
+    const MBCConfusionMetrics = indicator?.value
+    console.log(MBCConfusionMetrics)
     return (
         <>
             <div
@@ -99,6 +133,30 @@ export default function JobResult() {
                     gridGap: '16px',
                 }}
             >
+                {MBCConfusionMetrics &&
+                    Object.entries(MBCConfusionMetrics).map(([label, values]) => (
+                        <div>
+                            <h3>{label}</h3>
+                            <div
+                                style={{
+                                    display: 'grid',
+                                    gridTemplateColumns: 'repeat(7, 1fr)',
+                                    placeItems: 'center',
+                                    gridGap: '1px',
+                                    backgroundColor: '#fff',
+                                }}
+                            >
+                                {Object.entries(values).map(([k, v]) => (
+                                    <p style={{ fontWeight: 800, backgroundColor: '#fff' }}>
+                                        {String(k).toUpperCase()}
+                                    </p>
+                                ))}
+                                {Object.entries(values).map(([k, v]) => (
+                                    <p>{v}</p>
+                                ))}
+                            </div>
+                        </div>
+                    ))}
                 <Card>
                     <React.Suspense fallback={<Spinner />}>
                         <PlotlyVisualizer data={plotlyData} />
