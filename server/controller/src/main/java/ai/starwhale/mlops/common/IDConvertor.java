dd891c509cd79a83d482f6b705fad763f2b937da
dreamlandliu <205407@qq.com>
2022-04-22 14:17:46 +0800

    Update IDConvertor.java

--- server/controller/src/main/java/ai/starwhale/mlops/common/IDConvertor.java
+++ server/controller/src/main/java/ai/starwhale/mlops/common/IDConvertor.java
@@ -8,9 +8,7 @@
 package ai.starwhale.mlops.common;
 
 import ai.starwhale.mlops.exception.ConvertException;
-import java.util.Objects;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Component;
 
 @Slf4j
