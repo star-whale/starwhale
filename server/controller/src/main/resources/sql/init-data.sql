6526a11550662168139161fa6c5650d2495627f3
DreamlandLiu <205407@qq.com>
2022-04-25 15:47:11 +0800

    init-data.sql

--- server/controller/src/main/resources/sql/init-data.sql
+++ server/controller/src/main/resources/sql/init-data.sql
@@ -8,27 +8,22 @@
 #init data for test
 
 insert into user_role(role_name, role_name_en)
-values ('admin', 'admin');
+select 'admin', 'admin' from dual
+where not exists (select id from user_role where role_name='admin');
+
 insert into user_role(role_name, role_name_en)
-values ('user', 'user');
+select 'user', 'user' from dual
+where not exists (select id from user_role where role_name='user');
+
 
 #password=abcd1234
 insert into user_info(user_name, user_pwd, user_pwd_salt, role_id, user_enabled)
-values ('starwhale', 'ee9533077d01d2d65a4efdb41129a91e', '6ea18d595773ccc2beacce26', 1, 1);
+select 'starwhale', 'ee9533077d01d2d65a4efdb41129a91e', '6ea18d595773ccc2beacce26', 1, 1 from dual
+where not exists (select id from user_info where user_name='starwhale');
 
 #password=asdf7890
 insert into user_info(user_name, user_pwd, user_pwd_salt, role_id, user_enabled)
-values ('test', '7ce1c1d60c3393e4ca681e738036fe8c', 'be1866739033b7907631a71e', 1, 0);
+select 'test', '7ce1c1d60c3393e4ca681e738036fe8c', 'be1866739033b7907631a71e', 1, 0 from dual
+where not exists (select id from user_info where user_name='test');
 
-insert into project_info(project_name, owner_id)
-values ('project_for_test1', 1);
-insert into project_info(project_name, owner_id)
-values ('project_for_test2', 2);
-
-insert into agent_info(agent_ip, connect_time, agent_version, device_info)
-values (inet_aton('192.168.1.1'), '2022-03-20 15:00:30', 'v1.0.0', '{}');
-insert into agent_info(agent_ip, connect_time, agent_version, device_info)
-values (inet_aton('192.168.1.2'), '2022-03-20 15:10:32', 'v1.0.0', '{}');
-insert into agent_info(agent_ip, connect_time, agent_version, device_info)
-values (inet_aton('192.168.1.3'), '2022-03-21 15:10:32', 'v1.1.0', '{}');
 
