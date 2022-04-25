ee321583799c8d1ca5401806e7ce5fdacab6a608
gaoxinxing <15931259256@163.com>
2022-04-25 12:44:20 +0800

    feat:add report to white list

--- server/controller/src/main/java/ai/starwhale/mlops/configuration/ControllerProperties.java
+++ server/controller/src/main/java/ai/starwhale/mlops/configuration/ControllerProperties.java
@@ -3,8 +3,11 @@ package ai.starwhale.mlops.configuration;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import java.util.List;
+
 @Data
 @ConfigurationProperties(prefix = "sw.controller")
 public class ControllerProperties {
     private String apiPrefix = "/api/v1";
+    private List<String> whiteList;
 }
