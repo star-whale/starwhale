ab590f8da2939a0d4b481510886c0b812363d9bc
DreamlandLiu <205407@qq.com>
2022-05-05 16:55:03 +0800

    add user information in login response

--- server/controller/src/main/java/ai/starwhale/mlops/configuration/security/JwtLoginSuccessHandler.java
+++ server/controller/src/main/java/ai/starwhale/mlops/configuration/security/JwtLoginSuccessHandler.java
@@ -8,8 +8,10 @@
 package ai.starwhale.mlops.configuration.security;
 
 import ai.starwhale.mlops.api.protocol.Code;
+import ai.starwhale.mlops.api.protocol.user.UserVO;
 import ai.starwhale.mlops.common.util.JwtTokenUtil;
 import ai.starwhale.mlops.domain.user.User;
+import ai.starwhale.mlops.domain.user.UserService;
 import cn.hutool.json.JSONUtil;
 import java.io.IOException;
 import javax.annotation.Resource;
@@ -27,15 +29,20 @@ public class JwtLoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandle
     @Resource
     private JwtTokenUtil jwtTokenUtil;
 
+    @Resource
+    private UserService userService;
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
         User user = (User) authentication.getPrincipal();
         String jwtToken = jwtTokenUtil.generateAccessToken(user);
+        UserVO vo = userService.findUserById(user.getId());
+
         response.setContentType("application/json;charset=UTF-8");
         response.setHeader(HttpHeaders.AUTHORIZATION, String.format("Bearer %s", jwtToken));
         response.addHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS, HttpHeaders.AUTHORIZATION);
         response.setStatus(HttpStatus.OK.value());
-        response.getWriter().write(JSONUtil.toJsonStr(Code.success.asResponse("Login successful.")));
+        response.getWriter().write(JSONUtil.toJsonStr(Code.success.asResponse(vo)));
     }
 
 }
