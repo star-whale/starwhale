75e5af53612f5ffefd980086fd63250f94b6ed7d
tianwei <liutianweidlut@gmail.com>
2022-05-17 15:05:11 +0800

    feat: add roc/auc for multi classification report (#340)
    * add probality for ppl
    
    * add roc/auc for cmp report
    
    * update flake8 and black exclude dirs

--- client/starwhale/api/_impl/metric.py
+++ client/starwhale/api/_impl/metric.py
@@ -2,12 +2,14 @@ from __future__ import annotations
 import typing as t
 from functools import wraps
 
-from sklearn.metrics import (
+from sklearn.metrics import (  # type: ignore
     classification_report,
     multilabel_confusion_matrix,
     confusion_matrix,
     hamming_loss,
     cohen_kappa_score,
+    roc_curve,
+    auc,
 )
 
 
@@ -19,12 +21,19 @@ def multi_classification(
     confusion_matrix_normalize: str = "all",
     show_hamming_loss: bool = True,
     show_cohen_kappa_score: bool = True,
-    all_labels: t.Union[list[t.Any], None] = None,
-):
-    def _decorator(func):
+    show_roc_auc: bool = True,
+    all_labels: t.Optional[t.List[t.Any]] = None,
+) -> t.Any:
+    def _decorator(func: t.Any) -> t.Any:
         @wraps(func)
-        def _wrapper(*args, **kwargs):
-            y_true, y_pred = func(*args, **kwargs)
+        def _wrapper(*args: t.Any, **kwargs: t.Any) -> t.Dict[str, t.Any]:
+            y_pr: t.Any = None
+
+            _rt = func(*args, **kwargs)
+            if show_roc_auc:
+                y_true, y_pred, y_pr = _rt
+            else:
+                y_true, y_pred = _rt
 
             _r: t.Dict[str, t.Any] = {"kind": MetricKind.MultiClassification}
             cr = classification_report(
@@ -34,7 +43,7 @@ def multi_classification(
             _r["summary"] = {k: cr.get(k) for k in _summary_m}
             _r["labels"] = {k: v for k, v in cr.items() if k not in _summary_m}
 
-            # TODO: tune performace, use intermediate result
+            # TODO: tune performace, use intermediated result
             cm = confusion_matrix(
                 y_true, y_pred, labels=all_labels, normalize=confusion_matrix_normalize
             )
@@ -48,9 +57,32 @@ def multi_classification(
             if show_cohen_kappa_score:
                 _r["summary"]["cohen_kappa_score"] = cohen_kappa_score(y_true, y_pred)
 
-            # TODO: add roc/auc metric
+            if show_roc_auc and all_labels is not None and y_true and y_pr:
+                _r["roc_auc"] = {}
+                for _idx, _label in enumerate(all_labels):
+                    _r["roc_auc"][_label] = _calculate_roc_auc(
+                        y_true, y_pr, _label, _idx
+                    )
             return _r
 
         return _wrapper
 
     return _decorator
+
+
+def _calculate_roc_auc(
+    y_pred: t.List[t.Any],
+    y_pr: t.List[t.Any],
+    label: t.Any,
+    idx: int,
+) -> t.Dict[str, t.Any]:
+    # TODO: add sample rate
+    y_bin_true = [int(i == label) for i in y_pred]
+    y_label_pr = [m[idx] for m in y_pr]
+    fpr, tpr, thresholds = roc_curve(y_bin_true, y_label_pr)
+    return dict(
+        fpr=fpr.tolist(),
+        tpr=tpr.tolist(),
+        auc=auc(fpr, tpr),
+        thresholds=thresholds.tolist(),
+    )
