bd5d144f05db9db2a94b7a2de0bc91754059fa15
waynelwz <100347187+waynelwz@users.noreply.github.com>
2022-06-01 11:26:15 +0800

    chore: separate homepage & docs just keep docs (#448)
    * chore: package.json
    
    * chore: starwhale spell
    
    * chore: auto format
    
    * enhance: framework upgrade, home page ( hero/tab)
    
    * feat: homepage with try/user
    
    * feat: homepage try & footer component
    
    * feat: tab interactive
    
    * feat: footer social media/icon font update
    
    * feat: make tab section configable
    
    * feat: new icons
    
    * feat: new page contact us
    
    * Update desc Starwhale Project
    
    * feat: main page intergate section done
    
    * Update sectionUsers
    
    * feat: add notice page
    
    * remove: docs from this repo
    
    * Revert "remove: docs from this repo"
    
    This reverts commit 7cd7833a592f2b8ee51de408ec39c394a853b7f6.
    
    * remove: homepage part moved to other repo
    
    Co-authored-by: lijing-susan <101299635+lijing-susan@users.noreply.github.com>

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -1,128 +1,176 @@
-const path = require('path');
-
+const path = require("path");
 
 module.exports = {
-    title: 'Starwhale',
-    tagline: 'A MLOps Tool for your model evaluation',
-    url: 'https://starwhale.ai',
-    baseUrl: '/starwhale/',
-    onBrokenLinks: 'warn',
-    onBrokenMarkdownLinks: 'warn',
-    favicon: 'img/favicon.ico',
-    organizationName: 'star-whale.github.io',
-    projectName: 'starwhale',
-    i18n: {
-        defaultLocale: 'en',
-        locales: ['en', 'zh'],
+  title: "Starwhale",
+  tagline: "A MLOps Platform for Model Evaluation",
+  url: "https://starwhale.ai",
+  baseUrl: "/starwhale/",
+  onBrokenLinks: "warn",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "star-whale.github.io",
+  projectName: "starwhale",
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+  themeConfig: {
+    prism: {
+      theme: require("prism-react-renderer/themes/dracula"),
     },
-    themeConfig: {
-        prism: {
-            theme: require('prism-react-renderer/themes/dracula'),
-            darkTheme: require('prism-react-renderer/themes/dracula'),
-        },
-        zoomSelector: '.markdown :not(em) > img',
-        announcementBar: {
-            id: 'supportus',
-            content: 'If you like <b>Starwhale</b>, <a target="_blank" rel="noopener noreferrer" href="https://github.com/star-whale/starwhale">give us a star on GitHub!</a> 🌺',
-            isCloseable: true,
-        },
-        navbar: {
-            style: 'dark',
-            hideOnScroll: true,
-            title: ' ',
-            logo: {
-                alt: 'Starwhale',
-                src: 'img/logo_72x72px.png',
-            },
-            items: [
-                {
-                    to: '/docs/getting_started',
-                    label: 'Docs',
-                    position: 'left',
-                },
-                {
-                    to: '/docs/examples/mnist',
-                    label: 'Examples',
-                    position: 'left',
-                },
-                {
-                    label: 'References',
-                    position: 'left',
-                    items: [
-                        {
-                            label: 'Python SDK',
-                            to: '/docs/references/sdk',
-                        },
-                        {
-                            label: 'Server API',
-                            to: '/docs/references/api',
-                        },
-                        {
-                            label: 'CLI',
-                            to: '/docs/client_user_guide',
-                        },
-                    ],
-                },
-                {
-                    to: '/docs/faq',
-                    label: 'FAQ',
-                    position: 'left',
-                },
-                {
-                    href: 'https://starwhale.slack.com',
-                    position: 'right',
-                    className: 'header-ico header-ico--slack',
-                    'aria-label': 'Slack Channel',
-                },
-                {
-                    href: 'https://github.com/star-whale/starwhale',
-                    position: 'right',
-                    className: 'header-ico header-ico--github',
-                    'aria-label': 'GitHub repository',
-                },
-                {
-                    type: 'localeDropdown',
-                    position: 'right',
-                },
-            ],
-        },
-        footer: {
-            style: 'light',
-            copyright: `Copyright © ${new Date().getFullYear()} starwhale.ai. Built with Docusaurus.`,
-        },
+    colorMode: {
+      disableSwitch: true,
     },
-    presets: [
-        [
-            '@docusaurus/preset-classic',
-            {
-                docs: {
-                    sidebarPath: require.resolve('./sidebars.js'),
-                    editUrl:
-                        'https://github.com/star-whale/starwhale/edit/main/',
-                },
-                theme: {
-                    customCss: require.resolve('./src/css/custom.css'),
-                },
-                gtag: {
-                    trackingID: 'none',
-                    anonymizeIP: true,
-                },
-            },
-        ],
+    zoomSelector: ".markdown :not(em) > img",
+    announcementBar: {
+      id: "supportus",
+      content:
+        'If you like <b>Starwhale</b>, <a target="_blank" rel="noopener noreferrer" href="https://github.com/star-whale/starwhale">give us a star on GitHub!</a> 🌺',
+      isCloseable: true,
+    },
+    navbar: {
+      style: "dark",
+      hideOnScroll: true,
+      title: " ",
+      logo: {
+        alt: "Starwhale",
+        src: "img/starwhale-white.png",
+      },
+      items: [
+        {
+          to: "/docs/getting_started",
+          label: "Documentation",
+          position: "left",
+        },
+        {
+          to: "/docs/faq",
+          label: "FAQ",
+          position: "left",
+        },
+        {
+          to: "https://github.com/star-whale/starwhale",
+          position: "right",
+          className: "header-ico header-ico--github",
+          "aria-label": "GitHub repository",
+          label: "Github",
+        },
+        {
+          to: "https://starwhale.slack.com",
+          position: "right",
+          className: "header-ico header-ico--slack",
+          "aria-label": "Slack Channel",
+          label: "Slack",
+        },
+        // {
+        //   type: "localeDropdown",
+        //   position: "right",
+        // },
+      ],
+    },
+    footer: {
+      style: "light",
+      copyright: `Copyright © ${new Date().getFullYear()} Starwhale,Inc. All rights reserved. `,
+
+      links: [
+        // {
+        //   title: "Quickstart",
+        //   items: [
+        //     {
+        //       label: "Documentation",
+        //       to: "/docs",
+        //     },
+        //     {
+        //       label: "Examples",
+        //       to: "/docs/examples",
+        //     },
+        //     {
+        //       label: "Star us on Github",
+        //       to: "https://github.com/star-whale/starwhale",
+        //     },
+        //   ],
+        // },
+        // {
+        //   title: "Company",
+        //   items: [
+        //     {
+        //       label: "About Us",
+        //       to: "/docs/company/about",
+        //     },
+        //     {
+        //       label: "Contact Us",
+        //       to: "/docs/company/contact",
+        //     },
+        //   ],
+        // },
+      ],
+      // logo: {
+      //   alt: "Starwhale Open Source Logo",
+      //   src: "img/starwhale-white.png",
+      // },
+    },
+    custom: {
+      footerSocials: [
+        {
+          icon: "icon-Facebook",
+          to: "#",
+        },
+        {
+          icon: "icon-Twitter",
+          to: "#",
+        },
+        {
+          icon: "icon-Linkedin",
+          to: "#",
+        },
+        {
+          icon: "icon-Instagram",
+          to: "#",
+        },
+        {
+          icon: "icon-Youtub",
+          to: "#",
+        },
+      ],
+    },
+  },
+  customFields: {
+    email: "contact@starwhale.ai",
+    description:
+      "Starwhale is a MLOps platform to manage machine learning projects, models and datasets.",
+  },
+  presets: [
+    [
+      "@docusaurus/preset-classic",
+      {
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          editUrl: "https://github.com/star-whale/starwhale/tree/main/docs",
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.scss"),
+        },
+        gtag: {
+          trackingID: "none",
+          anonymizeIP: true,
+        },
+      },
     ],
-    plugins: [
-        [
-            require.resolve("@easyops-cn/docusaurus-search-local"),
-            {
-                hashed: true,
-                indexDocs: true,
-                docsRouteBasePath: '/docs',
-                searchResultLimits: 8,
-                searchResultContextMaxLength: 50,
-                indexBlog: false,
-                indexPages: false,
-                language: "en",
-            }],
-        path.resolve(__dirname, 'src/zoom-plugin')
-    ],
+  ],
+  plugins: [
+    // [
+    //   require.resolve("@easyops-cn/docusaurus-search-local"),
+    //   {
+    //     hashed: true,
+    //     indexDocs: true,
+    //     docsRouteBasePath: "/docs",
+    //     searchResultLimits: 8,
+    //     searchResultContextMaxLength: 50,
+    //     indexBlog: false,
+    //     indexPages: false,
+    //     language: "en",
+    //   },
+    // ],
+    "docusaurus-plugin-sass",
+    path.resolve(__dirname, "src/zoom-plugin"),
+  ],
 };
--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -7,151 +7,144 @@
  * @format
  */
 
-import React from 'react';
-import clsx from 'clsx';
-import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import useBaseUrl from '@docusaurus/useBaseUrl';
-import styles from './styles.module.css';
+import React from "react";
+import clsx from "clsx";
+import Layout from "@theme/Layout";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import useBaseUrl from "@docusaurus/useBaseUrl";
+import styles from "./styles.module.css";
 
 const features = [
-    {
-        newline: '',
-        title: 'New Model Defination',
-        description: (
-            <>
-                <b>SWMP</b> is design for MLOps.
-            </>
-        ),
-    },
-    {
-        newline: '',
-        title: 'New Dataset Defination',
-        description: (
-            <>
-                <b>SWDS</b> is design for MLOps.
-            </>
-        ),
-    },
-    {
-        newline: '',
-        title: 'Model Evaluation MLOps',
-        description: (
-            <>
-                Model Evaluation is local or cluster.
-            </>
-        ),
-    },
-    {
-        newline: '',
-        title: 'Cluster Scaliabilty',
-        description: (
-            <>
-                Easy to scale in cluster.
-            </>
-        ),
-    },
-    {
-        newline: '',
-        title: 'Easy Deployment',
-        description: (
-            <>
-                One click <b>Model Evaluation</b> to your production Kubernetes or BareMental Cluster <b>directly from the UI</b>.
-            </>
-        ),
-    },
-    {
-        newline: '',
-        title: 'Zero Ops',
-        description: (
-            <>
-                No ops.
-            </>
-        ),
-    },
+  {
+    newline: "",
+    title: "New Model Defination",
+    description: (
+      <>
+        <b>SWMP</b> is design for MLOps.
+      </>
+    ),
+  },
+  {
+    newline: "",
+    title: "New Dataset Defination",
+    description: (
+      <>
+        <b>SWDS</b> is design for MLOps.
+      </>
+    ),
+  },
+  {
+    newline: "",
+    title: "Model Evaluation MLOps",
+    description: <>Model Evaluation is local or cluster.</>,
+  },
+  {
+    newline: "",
+    title: "Cluster Scaliabilty",
+    description: <>Easy to scale in cluster.</>,
+  },
+  {
+    newline: "",
+    title: "Easy Deployment",
+    description: (
+      <>
+        One click <b>Model Evaluation</b> to your production Kubernetes or
+        BareMental Cluster <b>directly from the UI</b>.
+      </>
+    ),
+  },
+  {
+    newline: "",
+    title: "Zero Ops",
+    description: <>No ops.</>,
+  },
 ];
 
 function Feature({ imageUrl, title, description, newline }) {
-    const imgUrl = useBaseUrl(imageUrl);
-    return (
-        <div className={clsx(`col col--4 col--feature text--justified  ${newline}`, styles.feature)}>
-            {imgUrl && (
-                <div>
-                    <img className={styles.featureImage} src={imgUrl} alt={title} />
-                </div>
-            )}
-            <h3>{title}</h3>
-            <p>{description}</p>
+  const imgUrl = useBaseUrl(imageUrl);
+  return (
+    <div
+      className={clsx(
+        `col col--4 col--feature text--justified  ${newline}`,
+        styles.feature
+      )}
+    >
+      {imgUrl && (
+        <div>
+          <img className={styles.featureImage} src={imgUrl} alt={title} />
         </div>
-    );
+      )}
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </div>
+  );
 }
 
 function Home() {
-    const context = useDocusaurusContext();
-    const { siteConfig = {} } = context;
-    return (
-        <Layout
-            title={`Hello from ${siteConfig.title}`}
-            description="Description will go into a meta tag in <head />">
-            <header className={clsx('hero hero--primary', styles.heroBanner)}>
-                <div className="container">
-                    <h1 className={styles.heroProjectTagline}>
-                        <img
-                            alt={siteConfig.title}
-                            className={styles.heroLogo}
-                            src={useBaseUrl('/img/starwhale-white.png')}
-                        />
-                        <span
-                            className={styles.heroTitleTextHtml}
-                            dangerouslySetInnerHTML={{
-                                __html: siteConfig.tagline,
-                            }}
-                        />
-                    </h1>
-                    <div className={styles.indexCtas}>
-                        <Link className="button button--outline button--secondary button--lg button--rounded" to="/docs">
-                            Get Started
-                        </Link>
-                        <Link
-                            className="button button--primary button--lg button--rounded shadow--md"
-                            to="https://app.starwhale.ai">
-                            Free Sign Up
-                        </Link>
-                        <span className={styles.indexCtasGitHubButtonWrapper}>
-                            <iframe
-                                className={styles.indexCtasGitHubButton}
-                                src="https://ghbtns.com/github-btn.html?user=star-whale&amp;repo=starwhale&amp;type=star&amp;count=true&amp;size=large"
-                                width={160}
-                                height={30}
-                                title="GitHub Stars"
-                            />
-                        </span>
-                    </div>
-
-                </div>
-            </header>
-            <main>
-                {features && features.length > 0 && (
-                    <section className={styles.features}>
-                        <div className="container">
-                            <div className="row">
-                                {features.map(({ title, imageUrl, description, newline }) => (
-                                    <Feature
-                                        key={title}
-                                        title={title}
-                                        imageUrl={imageUrl}
-                                        description={description}
-                                        newline={newline}
-                                    />
-                                ))}
-                            </div>
-                        </div>
-                    </section>
-                )}
-            </main>
-        </Layout>
-    );
+  const context = useDocusaurusContext();
+  const { siteConfig = {} } = context;
+  return (
+    <Layout
+      title={`Hello from ${siteConfig.title}`}
+      description="Description will go into a meta tag in <head />"
+    >
+      <header className={clsx("hero hero--primary", styles.heroBanner)}>
+        <div className="container">
+          <h1 className={styles.heroProjectTagline}>
+            <span
+              className={styles.heroTitleTextHtml}
+              dangerouslySetInnerHTML={{
+                __html: siteConfig.tagline,
+              }}
+            />
+          </h1>
+          <div className={styles.indexCtas}>
+            <Link
+              className="button button--outline button--secondary button--lg button--rounded"
+              to="/docs"
+            >
+              Get Started
+            </Link>
+            <Link
+              className="button button--primary button--lg button--rounded shadow--md"
+              to="https://app.starwhale.ai"
+            >
+              Free Sign Up
+            </Link>
+            <span className={styles.indexCtasGitHubButtonWrapper}>
+              <iframe
+                className={styles.indexCtasGitHubButton}
+                src="https://ghbtns.com/github-btn.html?user=star-whale&amp;repo=starwhale&amp;type=star&amp;count=true&amp;size=large"
+                width={160}
+                height={30}
+                title="GitHub Stars"
+              />
+            </span>
+          </div>
+        </div>
+      </header>
+      <main>
+        {features && features.length > 0 && (
+          <section className={styles.features}>
+            <div className="container">
+              <div className="row">
+                {features.map(({ title, imageUrl, description, newline }) => (
+                  <Feature
+                    key={title}
+                    title={title}
+                    imageUrl={imageUrl}
+                    description={description}
+                    newline={newline}
+                  />
+                ))}
+              </div>
+            </div>
+          </section>
+        )}
+      </main>
+    </Layout>
+  );
 }
 
 export default Home;
