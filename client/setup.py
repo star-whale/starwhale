98b20cf38c691e452eca4fa14e8826590c80a154
tianwei <liutianweidlut@gmail.com>
2022-06-13 10:20:20 +0800

    enhance: add runtime validator (#545)
    * use 0.0.0.dev as default version
    
    * add more runtime python environment check

--- client/setup.py
+++ client/setup.py
@@ -24,7 +24,7 @@ install_requires = [
 
 
 def _format_version() -> str:
-    _v = os.environ.get("PYPI_RELEASE_VERSION", "0.1.0.alpha1")
+    _v = os.environ.get("PYPI_RELEASE_VERSION", "0.0.0.dev")
     _v = _v.lstrip("v").replace("-", ".")
     _vs = _v.split(".", 3)
     if len(_vs) == 4:
