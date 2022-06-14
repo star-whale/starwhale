f8f12df0654be3ecab58332a85f8ceb0b141816e
tianwei <liutianweidlut@gmail.com>
2022-06-14 22:56:59 +0800

    change doc

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -1,137 +1,117 @@
 const path = require("path");
 
 module.exports = {
-  title: "Starwhale",
-  tagline: "A MLOps Platform for Model Evaluation",
-  url: "https://starwhale.ai",
-  baseUrl: "/starwhale/",
-  onBrokenLinks: "warn",
-  onBrokenMarkdownLinks: "warn",
-  favicon: "img/favicon.ico",
-  organizationName: "star-whale.github.io",
-  projectName: "starwhale",
-  i18n: {
-    defaultLocale: "en",
-    locales: ["en"],
-  },
-  themeConfig: {
-    prism: {
-      theme: require("prism-react-renderer/themes/dracula"),
-    },
-    colorMode: {
-      disableSwitch: true,
-    },
-    zoomSelector: ".markdown :not(em) > img",
-    announcementBar: {
-      id: "supportus",
-      content:
-        'If you like <b>Starwhale</b>, <a target="_blank" rel="noopener noreferrer" href="https://github.com/star-whale/starwhale">give us a star on GitHub!</a> 🌺',
-      isCloseable: true,
-    },
-    navbar: {
-      style: "dark",
-      hideOnScroll: true,
-      title: " ",
-      logo: {
-        alt: "Starwhale",
-        src: "img/starwhale-white.png",
-      },
-      items: [
-        {
-          to: "/docs/getting_started",
-          label: "Documentation",
-          position: "left",
-        },
-        {
-          to: "/docs/faq",
-          label: "FAQ",
-          position: "left",
-        },
-        {
-          to: "https://github.com/star-whale/starwhale",
-          position: "right",
-          className: "header-ico header-ico--github",
-          "aria-label": "GitHub repository",
-          label: "Github",
-        },
-        {
-          to: "https://starwhale.slack.com",
-          position: "right",
-          className: "header-ico header-ico--slack",
-          "aria-label": "Slack Channel",
-          label: "Slack",
-        },
-        {
-          type: "localeDropdown",
-          position: "right",
-        },
-      ],
-    },
-    footer: {
-      style: "light",
-      copyright: `Copyright © ${new Date().getFullYear()} Starwhale,Inc. All rights reserved. `,
+    title: "Starwhale",
+    tagline: "A MLOps Platform for Model Evaluation",
+    url: "https://starwhale.ai",
+    baseUrl: "/starwhale/",
+    onBrokenLinks: "warn",
+    onBrokenMarkdownLinks: "warn",
+    favicon: "img/favicon.ico",
+    organizationName: "star-whale.github.io",
+    projectName: "starwhale",
 
-      links: [],
+    themeConfig: {
+        prism: {
+            theme: require("prism-react-renderer/themes/dracula"),
+        },
+        colorMode: {
+            disableSwitch: true,
+        },
+        zoomSelector: ".markdown :not(em) > img",
+        announcementBar: {
+            id: "supportus",
+            content:
+                'If you like <b>Starwhale</b>, <a target="_blank" rel="noopener noreferrer" href="https://github.com/star-whale/starwhale">give us a star on GitHub!</a> 🌺',
+            isCloseable: true,
+        },
+        navbar: {
+            style: "dark",
+            hideOnScroll: true,
+            title: " ",
+            logo: {
+                alt: "Starwhale",
+                src: "img/starwhale-white.png",
+            },
+            items: [
+                {
+                    to: "/docs/quickstart/main",
+                    label: "Documentation",
+                    position: "left",
+                },
+                {
+                    to: "/docs/faq",
+                    label: "FAQ",
+                    position: "left",
+                },
+                {
+                    to: "https://github.com/star-whale/starwhale",
+                    position: "right",
+                    className: "header-ico header-ico--github",
+                    "aria-label": "GitHub repository",
+                    label: "Github",
+                },
+                {
+                    to: "https://starwhale.slack.com",
+                    position: "right",
+                    className: "header-ico header-ico--slack",
+                    "aria-label": "Slack Channel",
+                    label: "Slack",
+                },
+            ],
+        },
+        footer: {
+            style: "light",
+            copyright: `Copyright © ${new Date().getFullYear()} Starwhale,Inc. All rights reserved. `,
+
+            links: [],
+        },
+        custom: {
+            footerSocials: [
+                {
+                    icon: "icon-Facebook",
+                    to: "#",
+                },
+                {
+                    icon: "icon-Twitter",
+                    to: "#",
+                },
+                {
+                    icon: "icon-Linkedin",
+                    to: "#",
+                },
+                {
+                    icon: "icon-Youtube",
+                    to: "#",
+                },
+            ],
+        },
     },
-    custom: {
-      footerSocials: [
-        {
-          icon: "icon-Facebook",
-          to: "#",
-        },
-        {
-          icon: "icon-Twitter",
-          to: "#",
-        },
-        {
-          icon: "icon-Linkedin",
-          to: "#",
-        },
-        {
-          icon: "icon-Youtube",
-          to: "#",
-        },
-      ],
+    customFields: {
+        email: "contact@starwhale.ai",
+        description:
+            "Starwhale is a MLOps platform to manage machine learning projects, models and datasets.",
     },
-  },
-  customFields: {
-    email: "contact@starwhale.ai",
-    description:
-      "Starwhale is a MLOps platform to manage machine learning projects, models and datasets.",
-  },
-  presets: [
-    [
-      "@docusaurus/preset-classic",
-      {
-        docs: {
-          sidebarPath: require.resolve("./sidebars.js"),
-          editUrl: "https://github.com/star-whale/starwhale/tree/main/docs",
-        },
-        theme: {
-          customCss: require.resolve("./src/css/custom.scss"),
-        },
-        gtag: {
-          trackingID: "none",
-          anonymizeIP: true,
-        },
-      },
+    presets: [
+        [
+            "@docusaurus/preset-classic",
+            {
+                docs: {
+                    sidebarPath: require.resolve("./sidebars.js"),
+                    editUrl: "https://github.com/star-whale/starwhale/tree/main/docs",
+                },
+                theme: {
+                    customCss: require.resolve("./src/css/custom.scss"),
+                },
+                gtag: {
+                    trackingID: "none",
+                    anonymizeIP: true,
+                },
+            },
+        ],
     ],
-  ],
-  plugins: [
-    // [
-    //   require.resolve("@easyops-cn/docusaurus-search-local"),
-    //   {
-    //     hashed: true,
-    //     indexDocs: true,
-    //     docsRouteBasePath: "/docs",
-    //     searchResultLimits: 8,
-    //     searchResultContextMaxLength: 50,
-    //     indexBlog: false,
-    //     indexPages: false,
-    //     language: "en",
-    //   },
-    // ],
-    "docusaurus-plugin-sass",
-    path.resolve(__dirname, "src/zoom-plugin"),
-  ],
+    plugins: [
+        "docusaurus-plugin-sass",
+        path.resolve(__dirname, "src/zoom-plugin"),
+    ],
 };
--- docs/sidebars.js
+++ docs/sidebars.js
@@ -1,9 +1,23 @@
 module.exports = {
     mainSidebar: [
         {
-            "Getting Started": ["getting_started"],
-            "Core Concepts": ["concepts"],
-            "User Guide": ["client_user_guide"],
+            "Quickstart": ["quickstart/main"],
+            "Tutorials": ["tutorials/main"],
+            "Fundamentals": ["fundamentals/concepts", "fundamentals/arch"],
+            "Standalone Guides": ["standalone/main"],
+            "Cloud Guides": ["cloud/main", "cloud/installation"],
+            "Python SDK Guides": ["sdk/main"],
+            "Contributing": ["contribute/main"],
+            "Reference": [
+                "reference/main",
+                {
+                    "Command Line Interface": [
+                        "reference/cli/main", "reference/cli/dataset", "reference/cli/model",
+                        "reference/cli/runtime", "reference/cli/project", "reference/cli/instance",
+                        "reference/cli/other"]
+                }
+            ],
+            "Releases": ["release/main", "release/changelog"],
         },
     ],
 };
