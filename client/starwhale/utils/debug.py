87883132acf9e9bbca69cf4664ea65fac3a92682
tianweidut <liutianweidlut@gmail.com>
2022-06-17 14:38:29 +0800

    update log format

--- client/starwhale/utils/debug.py
+++ client/starwhale/utils/debug.py
@@ -16,9 +16,9 @@ def init_logger(verbose: int) -> None:
         lvl = logging.INFO
     else:
         fmt = (
-            "<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | {elapsed} | "
-            "<level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> "
-            "- <level>{message}</level>"
+            "<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | "
+            "<level>{level: <6}</level> | "
+            "<level>{message}</level>"
         )
         lvl = logging.DEBUG
 
