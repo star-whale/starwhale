321a5bc90767c427fdde89c5081387006bfd1742
star <15031259256@163.com>
2022-06-20 14:27:45 +0800

    example: add nmt example new (#605)
    * add example
    
    * bug fix for nmt
    
    * bug fix for nmt
    
    * clear rm
    
    * add readme
    
    * remove main
    
    * remove data file
    
    Co-authored-by: gaoxinxing <15931259256@163.com>

---
+++ example/nmt/code/config.py
@@ -0,0 +1,4 @@
+SOS_token = 0
+EOS_token = 1
+
+MAX_LENGTH = 100
---
+++ example/nmt/code/helper.py
@@ -0,0 +1,81 @@
+import time
+import math
+import unicodedata
+import torch
+import re
+try:
+    from .config import MAX_LENGTH, EOS_token
+except ImportError:
+    from config import MAX_LENGTH, EOS_token
+
+
+def asMinutes(s):
+    m = math.floor(s / 60)
+    s -= m * 60
+    return '%dm %ds' % (m, s)
+
+
+def timeSince(since, percent):
+    now = time.time()
+    s = now - since
+    es = s / (percent)
+    rs = es - s
+    return '%s (- %s)' % (asMinutes(s), asMinutes(rs))
+
+
+
+eng_prefixes = (
+    "i am ", "i m ",
+    "he is", "he s ",
+    "she is", "she s ",
+    "you are", "you re ",
+    "we are", "we re ",
+    "they are", "they re "
+)
+
+
+def filterPair(p):
+    return len(p[0].split(' ')) < MAX_LENGTH and \
+        len(p[1].split(' ')) < MAX_LENGTH and \
+        p[1].startswith(eng_prefixes)
+
+
+def filterPairs(pairs):
+    return [pair for pair in pairs if filterPair(pair)]
+
+
+def indexesFromSentence(lang, sentence):
+    return [lang.word2index[word] for word in sentence.split(' ')]
+
+
+def tensorFromSentence(lang, sentence, device):
+    indexes = indexesFromSentence(lang, sentence)
+    indexes.append(EOS_token)
+    return torch.tensor(indexes, dtype=torch.long, device=device).view(-1, 1)
+
+
+def tensorsFromPair(input_lang, output_lang, pair, device):
+    input_tensor = tensorFromSentence(input_lang, pair[0], device)
+    target_tensor = tensorFromSentence(output_lang, pair[1], device)
+    return (input_tensor, target_tensor)
+
+
+# Turn a Unicode string to plain ASCII, thanks to
+# https://stackoverflow.com/a/518232/2809427
+def unicodeToAscii(s):
+    return ''.join(
+        c for c in unicodedata.normalize('NFD', s)
+        if unicodedata.category(c) != 'Mn'
+    )
+
+# Lowercase, trim, and remove non-letter characters
+
+
+def normalizeString(s):
+    s = unicodeToAscii(s.lower().strip())
+    s = re.sub(r"([.!?])", r" \1", s)
+    s = re.sub(r"[^a-zA-Z.!?]+", r" ", s)
+    return s
+
+def filterComment(s):
+    return s.startswith("CC-BY")
