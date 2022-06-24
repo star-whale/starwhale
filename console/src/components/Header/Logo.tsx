4c39c2f01f49cce2786a0cf1d854a22ff92e7ac4
waynelwz <wytheme.go@gmail.com>
2022-06-24 18:57:15 +0800

    update: make logo more smooth

--- console/src/components/Header/Logo.tsx
+++ console/src/components/Header/Logo.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { useCurrentUser } from '@/hooks/useCurrentUser'
 import smallLogoImg from '@/assets/logo_small_en_white.svg'
@@ -24,38 +24,51 @@ export interface ILogoProps extends IComposedComponentProps {
     expanded?: boolean
 }
 
+function preloadImage(src: string) {
+    return new Promise((resolve, reject) => {
+        const img = new Image()
+        img.onload = function () {
+            resolve(img)
+        }
+        img.onerror = img.onabort = function () {
+            reject(src)
+        }
+        img.src = src
+    })
+}
+
 export default function Logo({ expanded = true, className }: ILogoProps) {
     // eslint-disable-next-line react-hooks/exhaustive-deps
     const { currentUser } = useCurrentUser()
     const styles = useLogoStyles()
 
-    const LogoText = expanded ? (
-        <img style={{ width: '140px' }} src={normalLogoImg} alt='logo' />
-    ) : (
-        <img style={{ width: '50px' }} src={smallLogoImg} alt='logo' />
-    )
-
-    if (!currentUser)
-        return (
-            <div
-                className={classNames(styles.logoWrapper, className)}
-                style={{
-                    width: expanded ? sidebarExpandedWidth : sidebarFoldedWidth,
-                }}
-            >
-                {LogoText}
-            </div>
-        )
+    const Wrapper = currentUser ? Link : () => React.createElement('div')
 
     return (
-        <Link
+        <Wrapper
             className={classNames(styles.logoWrapper, className)}
             style={{
                 width: expanded ? sidebarExpandedWidth : sidebarFoldedWidth,
+                transition: 'all .2s ease',
             }}
             to='/'
         >
-            {LogoText}
-        </Link>
+            <img
+                style={{
+                    width: 140,
+                    display: expanded ? 'inline' : 'none',
+                }}
+                src={normalLogoImg}
+                alt='logo'
+            />
+            <img
+                style={{
+                    width: 50,
+                    display: !expanded ? 'inline' : 'none',
+                }}
+                src={smallLogoImg}
+                alt='logo'
+            />
+        </Wrapper>
     )
 }
