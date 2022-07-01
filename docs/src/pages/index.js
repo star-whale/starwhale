7fa23d6951f10aaaf98c5633a8a73cbb4d912110
waynelwz <wytheme.go@gmail.com>
2022-07-01 15:06:01 +0800

    feat: mobile ui for docs

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -16,68 +16,67 @@ import useBaseUrl from "@docusaurus/useBaseUrl";
 import styles from "./styles.module.css";
 import "./index.scss";
 
-
 function Feature({ imageUrl, title, description, newline }) {
-    const imgUrl = useBaseUrl(imageUrl);
-    return (
-        <div
-            className={clsx(
-                `col col--4 col--feature text--justified  ${newline}`,
-                styles.feature
-            )}
-        >
-            {imgUrl && (
-                <div>
-                    <img className={styles.featureImage} src={imgUrl} alt={title} />
-                </div>
-            )}
-            <h3>{title}</h3>
-            <p>{description}</p>
+  const imgUrl = useBaseUrl(imageUrl);
+  return (
+    <div
+      className={clsx(
+        `col col--4 col--feature text--justified  ${newline}`,
+        styles.feature
+      )}
+    >
+      {imgUrl && (
+        <div>
+          <img className={styles.featureImage} src={imgUrl} alt={title} />
         </div>
-    );
+      )}
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </div>
+  );
 }
 
 function Home() {
-    const context = useDocusaurusContext();
-    const { siteConfig = {} } = context;
-    return (
-        <Layout
-            title={`Docs`}
-            description="Description will go into a meta tag in <head />"
-        >
-            <div className="swMain-wrapper">
-                <header className={clsx("hero hero--primary", styles.heroBanner)}>
-                    <div className="container">
-                        <h1 className={styles.heroProjectTagline}>
-                            <span
-                                className={styles.heroTitleTextHtml}
-                                dangerouslySetInnerHTML={{
-                                    __html: siteConfig.tagline,
-                                }}
-                            />
-                        </h1>
-                        <div className={styles.indexCtas}>
-                            <Link
-                                className="button button--outline button--secondary button--lg button--rounded"
-                                to="/docs/quickstart/standalone"
-                            >
-                                Get Started
-                            </Link>
-                            <span className={styles.indexCtasGitHubButtonWrapper}>
-                                <iframe
-                                    className={styles.indexCtasGitHubButton}
-                                    src="https://ghbtns.com/github-btn.html?user=star-whale&amp;repo=starwhale&amp;type=star&amp;count=true&amp;size=large"
-                                    width={160}
-                                    height={30}
-                                    title="GitHub Stars"
-                                />
-                            </span>
-                        </div>
-                    </div>
-                </header>
+  const context = useDocusaurusContext();
+  const { siteConfig = {} } = context;
+  return (
+    <Layout
+      title={`Docs`}
+      description="Description will go into a meta tag in <head />"
+    >
+      <div className="swMain-wrapper bg-wrapper">
+        <header className={clsx("hero hero--primary", styles.heroBanner)}>
+          <div className="container">
+            <h1 className={styles.heroProjectTagline}>
+              <span
+                className={styles.heroTitleTextHtml}
+                dangerouslySetInnerHTML={{
+                  __html: siteConfig.tagline,
+                }}
+              />
+            </h1>
+            <div className={styles.indexCtas}>
+              <Link
+                className="button button--outline button--secondary button--lg button--rounded"
+                to="/docs/quickstart/standalone"
+              >
+                Get Started
+              </Link>
+              <span className={styles.indexCtasGitHubButtonWrapper}>
+                <iframe
+                  className={styles.indexCtasGitHubButton}
+                  src="https://ghbtns.com/github-btn.html?user=star-whale&amp;repo=starwhale&amp;type=star&amp;count=true&amp;size=large"
+                  width={160}
+                  height={30}
+                  title="GitHub Stars"
+                />
+              </span>
             </div>
-        </Layout>
-    );
+          </div>
+        </header>
+      </div>
+    </Layout>
+  );
 }
 
 export default Home;
