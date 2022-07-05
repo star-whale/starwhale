c05d73b5f6ccb9d489aed6354d9a94a9f7c83b89
liuyunxi <205407@qq.com>
2022-07-05 17:04:32 +0800

    pytest: e2e api test

--- scripts/apitest/pytest/test_project.py
+++ scripts/apitest/pytest/test_project.py
@@ -9,38 +9,87 @@ class TestProject:
         hu.login(host, port)
 
     def test_list(self, host, port):
-        res = requests.get(url=hu.url(host, port) + "/project",
+        res = requests.get(url=hu.url(host, port) + '/project',
                            headers=hu.header())
         response = res.json()
 
         assert res.status_code == 200
-        assert response["code"] == "success"
-        assert len(response["data"]["list"]) > 0
+        assert response['code'] == 'success'
+        assert len(response['data']['list']) > 0
 
-        print("Test project list ok.")
+        print('Test project list ok.')
 
     def test_create(self, host, port):
-        res = requests.post(url=hu.url(host, port) + "/project",
-                            json={"projectName": "project_for_test"},
+        res = requests.post(url=hu.url(host, port) + '/project',
+                            json={'projectName': 'project_for_test'},
                             headers=hu.header())
         response = res.json()
 
         assert res.status_code == 200
-        assert response["code"] == "success"
+        assert response['code'] == 'success'
 
-        print("Test Create project ok.")
+        print('Test Project Create ok.')
+
+    def test_info(self, host, port):
+        res = requests.get(url=hu.url(host, port) + '/project/project_for_test',
+                           headers=hu.header())
+        response = res.json()
+
+        assert res.status_code == 200
+        assert response['code'] == 'success'
+        assert response['data']['name'] == 'project_for_test'
+
+        print('Test Project get information ok.')
+
+    def test_modify(self, host, port):
+        res = requests.put(url=hu.url(host, port) + '/project'
+                                                    '/project_for_test',
+                           json={'projectName': 'project_test_modify'},
+                           headers=hu.header())
+        response = res.json()
+
+        assert res.status_code == 200
+        assert response['code'] == 'success'
+
+        print('Test Project Modify ok.')
 
     def test_remove(self, host, port):
-        res = requests.delete(url=hu.url(host, port) + "/project"
-                                                       "/project_for_test",
+        res = requests.delete(url=hu.url(host, port) + '/project'
+                                                       '/project_test_modify',
                               headers=hu.header())
         response = res.json()
 
         assert res.status_code == 200
-        assert response["code"] == "success"
+        assert response['code'] == 'success'
 
-        print("Test Remove project ok.")
+        print('Test Project Remove ok.')
+
+    def test_recover(self, host, port):
+        res = requests.post(url=hu.url(host, port) + '/project',
+                            json={'projectName': 'project_for_recover'},
+                            headers=hu.header())
+        response = res.json()
+        data = response['data']
+
+        requests.delete(url=hu.url(host, port) + '/project'
+                                                 '/' + data,
+                        headers=hu.header())
+
+        res = requests.put(url=hu.url(host, port) + '/project'
+                                                    '/' + data +
+                                                    '/recover',
+                           headers=hu.header())
+        response = res.json()
+
+        assert res.status_code == 200
+        assert response['code'] == 'success'
+
+        requests.delete(url=hu.url(host, port) + '/project'
+                                                 '/' + data,
+                        headers=hu.header())
+
+        print('Test Project Recover ok.')
 
 
 if __name__ == '__main__':
-    pytest.main(["test_project.py"])
+    pytest.main(['test_project.py'])
--- scripts/apitest/pytest/test_user.py
+++ scripts/apitest/pytest/test_user.py
@@ -9,16 +9,26 @@ class TestUser:
         hu.login(host, port)
 
     def test_list(self, host, port):
-        res = requests.get(url=hu.url(host, port) + "/user",
+        res = requests.get(url=hu.url(host, port) + '/user',
                            headers=hu.header())
         response = res.json()
 
         assert res.status_code == 200
-        assert response["code"] == "success"
-        assert len(response["data"]["list"]) > 0
+        assert response['code'] == 'success'
+        assert len(response['data']['list']) > 0
 
-        print("Test user list ok.")
+        print('Test User list ok.')
+
+    def test_current_user(self, host, port):
+        res = requests.get(url=hu.url(host, port) + '/user/current',
+                           headers=hu.header())
+        response = res.json()
+        assert res.status_code == 200
+        assert response['code'] == 'success'
+        assert response['data']['name'] == 'starwhale'
+
+        print('Test User Current ok.')
 
 
 if __name__ == '__main__':
-    pytest.main(["test_user.py"])
+    pytest.main(['test_user.py'])
