1b122822b699d490e5659905b166a3727cb5ddfa
jialei <jialeicui@126.com>
2022-07-12 13:02:05 +0800

    feat(client): board support show project tree and model list

--- client/starwhale/cli/__init__.py
+++ client/starwhale/cli/__init__.py
@@ -8,13 +8,13 @@ from starwhale.utils.debug import init_logger
 from starwhale.core.job.cli import job_cmd
 from starwhale.utils.config import load_swcli_config
 from starwhale.core.model.cli import model_cmd
+from starwhale.cli.board.board import open_board
 from starwhale.core.dataset.cli import dataset_cmd
 from starwhale.core.project.cli import project_cmd
 from starwhale.core.runtime.cli import runtime_cmd
 from starwhale.core.instance.cli import instance_cmd
 
 from .mngt import add_mngt_command
-from .board import open_board
 
 
 def create_sw_cli() -> click.core.Group:
