3620f9ff16326c5b113cddc661975dc26d745d5e
tianweidut <liutianweidlut@gmail.com>
2022-07-19 13:58:58 +0800

    add runtime example files

---
+++ example/runtime/pytorch/dummy/setup.py
@@ -0,0 +1,14 @@
+from importlib_metadata import entry_points
+from setuptools import setup, find_packages
+
+setup(
+    name="dummy",
+    description="Dummy package only for test",
+    version="0.0.0",
+    packages=find_packages(exclude=["ez_setup", "tests*"]),
+    include_package_data=True,
+    entry_points="""
+        [console_scripts]
+        dummy=dummy:cli
+    """,
+)
