113f55b7da6243b499a2ebcf0992629a60c98f3e
liuyunxi <205407@qq.com>
2022-07-26 18:49:35 +0800

    Project Role - security of jwt

--- server/controller/src/main/java/ai/starwhale/mlops/configuration/security/JwtAuthenticationProvider.java
+++ server/controller/src/main/java/ai/starwhale/mlops/configuration/security/JwtAuthenticationProvider.java
@@ -19,15 +19,9 @@ package ai.starwhale.mlops.configuration.security;
 import ai.starwhale.mlops.domain.user.bo.User;
 import ai.starwhale.mlops.domain.user.UserService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.authentication.AccountExpiredException;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.DisabledException;
-import org.springframework.security.authentication.LockedException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * User role verification specific implementation
@@ -44,14 +38,14 @@ public class JwtAuthenticationProvider implements AuthenticationProvider {
     @Override
     public Authentication authenticate(Authentication authentication)
         throws AuthenticationException {
-        UserDetails userDetails = userService.loadUserByUsername(authentication.getName());
+        User user = userService.loadUserByUsername(authentication.getName());
+        user.defaultChecks();
 
         JwtLoginToken jwtLoginToken = (JwtLoginToken) authentication;
-        defaultCheck(userDetails);
         // Username and password verification
-        additionalAuthenticationChecks(userDetails, jwtLoginToken);
+        user.additionalAuthenticationChecks(jwtLoginToken);
         // Create authenticated authentication
-        JwtLoginToken authenticatedToken = new JwtLoginToken(userDetails, jwtLoginToken.getCredentials(), userDetails.getAuthorities());
+        JwtLoginToken authenticatedToken = new JwtLoginToken(user, jwtLoginToken.getCredentials(), user.getAuthorities());
         authenticatedToken.setDetails(jwtLoginToken.getDetails());
         return authenticatedToken;
     }
@@ -60,43 +54,6 @@ public class JwtAuthenticationProvider implements AuthenticationProvider {
     public boolean supports(Class<?> authentication) {
         return (JwtLoginToken.class
             .isAssignableFrom(authentication));
-    }
-
-    /**
-     * Check for some default information
-     *
-     * @param user
-     */
-    private void defaultCheck(UserDetails user) {
-        if (!user.isAccountNonLocked()) {
-            throw new LockedException("User account is locked");
-        }
-
-        if (!user.isEnabled()) {
-            throw new DisabledException("User is disabled");
-        }
-
-        if (!user.isAccountNonExpired()) {
-            throw new AccountExpiredException("User account has expired");
-        }
-    }
-
-    /**
-     * Check if the password is correct
-     *
-     * @param userDetails
-     * @param authentication
-     * @throws AuthenticationException
-     */
-    private void additionalAuthenticationChecks(UserDetails userDetails, JwtLoginToken authentication) throws AuthenticationException {
-        if (authentication.getCredentials() == null) {
-            throw new BadCredentialsException("Bad credentials");
-        }
-        String presentedPassword = authentication.getCredentials().toString();
-        PasswordEncoder passwordEncoder = SWPasswordEncoder.getEncoder(((User)userDetails).getSalt());
-        if (!passwordEncoder.matches(presentedPassword, userDetails.getPassword())) {
-            throw new BadCredentialsException("Bad credentials");
-        }
     }
 
 }
--- server/controller/src/main/java/ai/starwhale/mlops/configuration/security/JwtTokenFilter.java
+++ server/controller/src/main/java/ai/starwhale/mlops/configuration/security/JwtTokenFilter.java
@@ -19,16 +19,26 @@ package ai.starwhale.mlops.configuration.security;
 import static ai.starwhale.mlops.common.util.HttpUtil.error;
 
 import ai.starwhale.mlops.api.protocol.Code;
+import ai.starwhale.mlops.common.util.HttpUtil;
+import ai.starwhale.mlops.common.util.HttpUtil.Resources;
 import ai.starwhale.mlops.common.util.JwtTokenUtil;
+import ai.starwhale.mlops.domain.user.bo.Role;
 import ai.starwhale.mlops.domain.user.bo.User;
 import ai.starwhale.mlops.domain.user.UserService;
+import ai.starwhale.mlops.exception.api.StarWhaleApiException;
 import io.jsonwebtoken.Claims;
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AccountStatusException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.util.StringUtils;
@@ -63,15 +73,42 @@ public class JwtTokenFilter extends OncePerRequestFilter {
         }
 
         String token = header.split(" ")[1].trim();
-
         if(!jwtTokenUtil.validate(token)) {
             error(httpServletResponse, HttpStatus.FORBIDDEN.value(), Code.accessDenied, "JWT token is expired or invalid.");
             return;
         }
 
+
         Claims claims = jwtTokenUtil.parseJWT(token);
 
         User user = userService.loadUserByUsername(jwtTokenUtil.getUsername(claims));
+        try {
+            user.defaultChecks();
+        } catch (AccountStatusException e) {
+            error(httpServletResponse, HttpStatus.FORBIDDEN.value(), Code.accessDenied, e.getMessage());
+            return;
+        }
+        // Get the roles of System
+        List<Role> sysRoles = userService.getProjectRolesOfUser(user, "0");
+        Set<Role> roles = sysRoles.stream().filter(
+            role -> role.getAuthority().equals("OWNER")).collect(Collectors.toSet());
+        // Get project roles
+        String projectUrl = httpServletRequest.getParameter("project");
+        if(projectUrl == null) {
+            projectUrl = httpServletRequest.getParameter("projectUrl");
+        }
+        if(projectUrl == null) {
+            projectUrl = HttpUtil.getResourceUrlFromPath(httpServletRequest.getRequestURI(), Resources.PROJECT);
+        }
+        try {
+            List<Role> rolesOfUser = userService.getProjectRolesOfUser(user,
+                projectUrl);
+            roles.addAll(rolesOfUser);
+        } catch (StarWhaleApiException e) {
+            logger.error(e.getMessage());
+        }
+        user.setRoles(roles);
+        // Build jwt token with user
         JwtLoginToken jwtLoginToken = new JwtLoginToken(user, "", user.getAuthorities());
         jwtLoginToken.setDetails(new WebAuthenticationDetails(httpServletRequest));
         SecurityContextHolder.getContext().setAuthentication(jwtLoginToken);
