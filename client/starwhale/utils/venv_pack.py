908a83469bffbf5d954e9a8f7d640924bb0b5012
tianweidut <liutianweidlut@gmail.com>
2022-07-26 10:12:32 +0800

    remove useless copy fs

--- client/starwhale/utils/venv_pack.py
+++ client/starwhale/utils/venv_pack.py
@@ -1,10 +1,9 @@
 import os
+import shutil
 import typing as t
 import tarfile
 import tempfile
 from pathlib import Path
-
-from fs.copy import copy_fs
 
 from starwhale.utils.error import FormatError, ExistedError
 
@@ -13,19 +12,21 @@ def pack(prefix: t.Union[str, Path], output: str, force: bool = False) -> None:
     if os.path.exists(output) and not force:
         raise ExistedError(output)
 
-    from .fs import empty_dir
     from .venv import check_valid_venv_prefix
 
     if not check_valid_venv_prefix(prefix):
         raise FormatError(f"venv prefix: {prefix}")
 
+    _, temp_fpath = tempfile.mkstemp(
+        prefix="starwhale-venv-", dir=os.path.dirname(output)
+    )
+
     # TODO: check editable packages
-    temp_dir = tempfile.mkdtemp(prefix="starwhale-venv-", dir=os.path.dirname(output))
-
     try:
-        copy_fs(str(prefix), temp_dir)
+        with tarfile.open(temp_fpath, "w:gz") as tar:
+            tar.add(str(prefix), arcname="")
 
-        with tarfile.open(output, "w:gz") as tar:
-            tar.add(str(temp_dir), arcname="")
+        shutil.move(temp_fpath, output)
     finally:
-        empty_dir(temp_dir)
+        if os.path.exists(temp_fpath):
+            os.unlink(temp_fpath)
