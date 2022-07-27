167aca6cb9f4ea0bd0b245282b24e6549336405e
tianweidut <liutianweidlut@gmail.com>
2022-07-27 16:05:28 +0800

    add proxy for docker build

--- scripts/e2e_test/start_test.sh
+++ scripts/e2e_test/start_test.sh
@@ -166,7 +166,7 @@ upload_pypi_to_nexus() {
 
 buid_runtime_image() {
   pushd ../../docker
-  docker build -t starwhale -f Dockerfile.starwhale --build-arg PIPY_REPO_E2E=true --build-arg PORT_NEXUS=$PORT_NEXUS --build-arg LOCAL_PYPI_HOSTNAME=$IP_DOCKER_BRIDGE --build-arg SW_VERSION=$PYPI_RELEASE_VERSION .
+  docker build -t starwhale -f Dockerfile.starwhale --build-arg ENABLE_E2E_TEST_PYPI_REPO=1 --build-arg PORT_NEXUS=$PORT_NEXUS --build-arg LOCAL_PYPI_HOSTNAME=$IP_DOCKER_BRIDGE --build-arg SW_VERSION=$PYPI_RELEASE_VERSION .
   docker tag starwhale $NEXUS_HOSTNAME:$PORT_NEXUS_DOCKER/starwhale:$PYPI_RELEASE_VERSION
   popd
 }
