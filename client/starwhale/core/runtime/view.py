5b8dbe2e423fe28206b751f3962f33d69b4e0481
tianweidut <liutianweidlut@gmail.com>
2022-08-02 10:46:00 +0800

    add extract for standalone runtime restore

--- client/starwhale/core/runtime/view.py
+++ client/starwhale/core/runtime/view.py
@@ -169,9 +169,11 @@ class RuntimeTermView(BaseTermView):
         if in_production() or (os.path.exists(target) and os.path.isdir(target)):
             workdir = Path(target)
         else:
-            uri = URI(target, URIType.RUNTIME)
-            store = RuntimeStorage(uri)
-            workdir = store.snapshot_workdir
+            _uri = URI(target, URIType.RUNTIME)
+            _runtime = StandaloneRuntime(_uri)
+            workdir = _runtime.store.snapshot_workdir
+            if not workdir.exists():
+                _runtime.extract(force=True, target=workdir)
 
         console.print(
             f":golfer: try to restore python runtime environment: {workdir} ..."
