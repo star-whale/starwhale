78df99aaea082e8e963558a1ab4855bb85e88d1b
gaoxinxing <15931259256@163.com>
2022-08-15 11:02:46 +0800

    remove some comment

--- server/controller-sdk/src/main/java/ai/starwhale/mlops/domain/task/TaskType.java
+++ server/controller-sdk/src/main/java/ai/starwhale/mlops/domain/task/TaskType.java
@@ -19,7 +19,6 @@ package ai.starwhale.mlops.domain.task;
 /**
  * inference task type,the complete process includes ppl and result
  */
-@Deprecated
 public enum TaskType {
     UNKNOWN(-1), PPL(1), CMP(2);
     final int value;
