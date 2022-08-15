a850a2118d3dee5d76ee43ff807dd3af9182b539
gaoxinxing <15931259256@163.com>
2022-08-15 09:27:40 +0800

    unit test code fix

--- client/tests/sdk/test_metric.py
+++ client/tests/sdk/test_metric.py
@@ -1,30 +1,31 @@
 import random
-from unittest import skip
+from unittest import skip, TestCase
 
 from starwhale.api._impl.metric import multi_classification
 
 
 @skip
-def test_multi_classification_metric():
-    def _cmp():
-        return (
-            [1, 2, 3, 4, 5, 6, 7, 8, 9],
-            [1, 3, 2, 4, 5, 6, 7, 8, 9],
-            [[1 / random.randint(1, 10) for i in range(1, 10)] for i in range(1, 10)],
-        )
+class TestMultiClassificationMetric(TestCase):
+    def test_multi_classification_metric(self):
+        def _cmp():
+            return (
+                [1, 2, 3, 4, 5, 6, 7, 8, 9],
+                [1, 3, 2, 4, 5, 6, 7, 8, 9],
+                [[1 / random.randint(1, 10) for i in range(1, 10)] for i in range(1, 10)],
+            )
 
-    rt = multi_classification(
-        confusion_matrix_normalize="all",
-        show_hamming_loss=True,
-        show_cohen_kappa_score=True,
-        show_roc_auc=True,
-        all_labels=[i for i in range(1, 10)],
-    )(_cmp)()
+        rt = multi_classification(
+            confusion_matrix_normalize="all",
+            show_hamming_loss=True,
+            show_cohen_kappa_score=True,
+            show_roc_auc=True,
+            all_labels=[i for i in range(1, 10)],
+        )(_cmp)()
 
-    assert rt["kind"] == "multi_classification"
-    assert "accuracy" in rt["summary"]
-    assert "macro_avg" in rt["summary"]
-    assert len(rt["labels"]) == 9
-    assert "binarylabel" in rt["confusion_matrix"]
-    assert "multilabel" in rt["confusion_matrix"]
-    assert len(rt["roc_auc"]) == 9
+        assert rt["kind"] == "multi_classification"
+        assert "accuracy" in rt["summary"]
+        assert "macro_avg" in rt["summary"]
+        assert len(rt["labels"]) == 9
+        assert "binarylabel" in rt["confusion_matrix"]
+        assert "multilabel" in rt["confusion_matrix"]
+        assert len(rt["roc_auc"]) == 9
