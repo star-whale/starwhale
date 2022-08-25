a2ba42cccdf2877e9a469647942bcebf208b851e
gaoxinxing <15931259256@163.com>
2022-08-25 15:01:04 +0800

    fix  unit test error

--- server/controller/src/test/java/ai/starwhale/mlops/domain/job/JobBoConverterTest.java
+++ server/controller/src/test/java/ai/starwhale/mlops/domain/job/JobBoConverterTest.java
@@ -26,6 +26,7 @@ import ai.starwhale.mlops.domain.job.mapper.JobSWDSVersionMapper;
 import ai.starwhale.mlops.domain.job.po.JobEntity;
 import ai.starwhale.mlops.domain.job.status.JobStatus;
 import ai.starwhale.mlops.domain.node.Device.Clazz;
+import ai.starwhale.mlops.domain.project.po.ProjectEntity;
 import ai.starwhale.mlops.domain.runtime.mapper.RuntimeMapper;
 import ai.starwhale.mlops.domain.runtime.mapper.RuntimeVersionMapper;
 import ai.starwhale.mlops.domain.runtime.po.RuntimeEntity;
@@ -59,6 +60,8 @@ public class JobBoConverterTest {
 
         JobEntity jobEntity = JobEntity.builder()
             .id(1L)
+            .projectId(1L)
+            .project(ProjectEntity.builder().id(1L).projectName("test-project").build())
             .deviceAmount(1)
             .deviceType(Clazz.CPU.getValue())
             .jobStatus(JobStatus.RUNNING)
