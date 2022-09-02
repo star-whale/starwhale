905d1291605eaa1a78e347663f584207d05a6a82
waynelwz <wytheme.go@gmail.com>
2022-09-02 11:50:17 +0800

    fix: use versionUrl as default id

--- console/src/domain/runtime/services/runtime.ts
+++ console/src/domain/runtime/services/runtime.ts
@@ -16,7 +16,7 @@ export async function fetchRuntime(projectId: string, runtimeId: string): Promis
 
 export async function revertRuntime(projectId: string, runtimeId: string, runtimeVersionId: string): Promise<any> {
     const resp = await axios.post<IRuntimeDetailSchema>(`/api/v1/project/${projectId}/runtime/${runtimeId}/revert`, {
-        versionId: runtimeVersionId,
+        versionUrl: runtimeVersionId,
     })
     return resp.data
 }
