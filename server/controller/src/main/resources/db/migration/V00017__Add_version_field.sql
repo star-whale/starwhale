fcd87a69c95633e8b223eefed781bde591a10bd5
liuyunxi <205407@qq.com>
2022-09-05 11:51:50 +0800

    merge: alter flyway sql file name

---
+++ server/controller/src/main/resources/db/migration/V00017__Add_version_field.sql
@@ -0,0 +1,20 @@
+alter table swmp_version
+    add version_order bigint default 0 null after id;
+update swmp_version set version_order = id;
+create index idx_version_order
+    on swmp_version (version_order);
+
+alter table dataset_version
+    add version_order bigint default 0 not null after id;
+update dataset_version set version_order = id;
+create index idx_version_order
+    on dataset_version (version_order);
+
+alter table runtime_version
+    add version_order bigint default 0 not null after id;
+update runtime_version set version_order = id;
+create index idx_version_order
+    on runtime_version (version_order);
+
+
+
