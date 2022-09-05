e7f0d7e9108e5f05a615bb501a6d9dc309a191a7
liuyunxi <205407@qq.com>
2022-09-05 15:26:24 +0800

    fix: alter flyway sql

--- server/controller/src/main/resources/db/migration/V00017__Add_version_field.sql
+++ server/controller/src/main/resources/db/migration/V00017__Add_version_field.sql
@@ -1,5 +1,5 @@
 alter table swmp_version
-    add version_order bigint default 0 null after id;
+    add version_order bigint default 0 not null after id;
 update swmp_version set version_order = id;
 create index idx_version_order
     on swmp_version (version_order);
