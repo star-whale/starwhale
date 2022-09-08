3ac7be7bf69d4321f1b587f482f2fa269bb659cb
liuyunxi <205407@qq.com>
2022-09-08 20:45:04 +0800

    unit test for user classes

---
+++ server/controller/src/test/java/ai/starwhale/mlops/api/UserControllerTest.java
@@ -0,0 +1,321 @@
+/*
+ * Copyright 2022 Starwhale, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package ai.starwhale.mlops.api;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.emptyIterable;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.hasProperty;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.iterableWithSize;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.ArgumentMatchers.same;
+import static org.mockito.BDDMockito.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.mock;
+
+import ai.starwhale.mlops.api.protocol.user.RoleVo;
+import ai.starwhale.mlops.api.protocol.user.SystemRoleVo;
+import ai.starwhale.mlops.api.protocol.user.UserCheckPasswordRequest;
+import ai.starwhale.mlops.api.protocol.user.UserRequest;
+import ai.starwhale.mlops.api.protocol.user.UserRoleAddRequest;
+import ai.starwhale.mlops.api.protocol.user.UserRoleDeleteRequest;
+import ai.starwhale.mlops.api.protocol.user.UserRoleUpdateRequest;
+import ai.starwhale.mlops.api.protocol.user.UserRoleVo;
+import ai.starwhale.mlops.api.protocol.user.UserUpdatePasswordRequest;
+import ai.starwhale.mlops.api.protocol.user.UserUpdateStateRequest;
+import ai.starwhale.mlops.api.protocol.user.UserVo;
+import ai.starwhale.mlops.common.IdConvertor;
+import ai.starwhale.mlops.common.util.JwtTokenUtil;
+import ai.starwhale.mlops.domain.project.ProjectService;
+import ai.starwhale.mlops.domain.user.UserService;
+import ai.starwhale.mlops.domain.user.bo.User;
+import ai.starwhale.mlops.exception.api.StarwhaleApiException;
+import com.github.pagehelper.PageInfo;
+import java.util.List;
+import java.util.Objects;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+
+public class UserControllerTest {
+
+    private UserController controller;
+
+    private UserService userService;
+
+    private ProjectService projectService;
+
+    private JwtTokenUtil jwtTokenUtil;
+
+    @BeforeEach
+    public void setUp() {
+        jwtTokenUtil = mock(JwtTokenUtil.class);
+        projectService = mock(ProjectService.class);
+        userService = mock(UserService.class);
+        given(userService.currentUser())
+                .willReturn(UserVo.builder().id("1").name("current").build());
+        given(userService.currentUserDetail())
+                .willReturn(User.builder().id(1L).name("current").build());
+        given(userService.checkCurrentUserPassword("correct"))
+                .willReturn(true);
+        given(userService.changePassword(argThat(user -> user.getId() == 1L), same("newPassword")))
+                .willReturn(true);
+
+        IdConvertor idConvertor = new IdConvertor();
+        controller = new UserController(userService, projectService, idConvertor, jwtTokenUtil);
+    }
+
+    @Test
+    public void testListUser() {
+        UserVo u1 = UserVo.builder().id("1").name("u1").build();
+        UserVo u2 = UserVo.builder().id("2").name("u2").build();
+        given(userService.listUsers(any(User.class), any()))
+                .willReturn(PageInfo.of(List.of(u1, u2)));
+        given(userService.listUsers(argThat(user -> user.getName().equals("u1")), any()))
+                .willReturn(PageInfo.of(List.of(u1)));
+
+        var resp = controller.listUser("", 1, 10);
+        assertThat(resp.getStatusCode(), is(HttpStatus.OK));
+        assertThat(Objects.requireNonNull(resp.getBody()).getData(), allOf(
+                notNullValue(),
+                is(hasProperty("total", is(2L))),
+                is(hasProperty("list", is(iterableWithSize(2))))
+        ));
+
+        resp = controller.listUser("u1", 1, 10);
+        assertThat(resp.getStatusCode(), is(HttpStatus.OK));
+        assertThat(Objects.requireNonNull(resp.getBody()).getData(), allOf(
+                notNullValue(),
+                is(hasProperty("total", is(1L))),
+                is(hasProperty("list", is(iterableWithSize(1)))),
+                is(hasProperty("list", is(hasItem(hasProperty("name", is("u1"))))))
+        ));
+    }
+
+    @Test
+    public void testCreateUser() {
+        given(
+                userService.createUser(argThat(user -> user.getName().equals("name")), same("password"),
+                        same("salt")))
+                .willReturn(1L);
+
+        UserRequest request = new UserRequest();
+        request.setUserName("name");
+        request.setUserPwd("password");
+        request.setSalt("salt");
+        var resp = controller.createUser(request);
+        assertThat(resp.getStatusCode(), is(HttpStatus.OK));
+        assertThat(Objects.requireNonNull(resp.getBody()).getData(), is("1"));
+    }
+
+    @Test
+    public void testGetCurrentUser() {
+        var resp = controller.getCurrentUser();
+        assertThat(resp.getStatusCode(), is(HttpStatus.OK));
+        assertThat(Objects.requireNonNull(resp.getBody()).getData(), allOf(
+                notNullValue(),
+                is(hasProperty("name", is("current")))
+        ));
+    }
+
+    @Test
+    public void testGetCurrentUserRoles() {
+        given(userService.listCurrentUserRoles(same("p1")))
+                .willReturn(List.of(UserRoleVo.builder().id("1").build()));
+
+        var resp = controller.getCurrentUserRoles("p1");
+        assertThat(resp.getStatusCode(), is(HttpStatus.OK));
+        assertThat(Objects.requireNonNull(resp.getBody()).getData(), allOf(
+                notNullValue(),
+                is(iterableWithSize(1))
+        ));
+
+        resp = controller.getCurrentUserRoles("p2");
+        assertThat(resp.getStatusCode(), is(HttpStatus.OK));
+        assertThat(Objects.requireNonNull(resp.getBody()).getData(), allOf(
+                notNullValue(),
+                is(emptyIterable())
+        ));
+    }
+
+    @Test
+    public void testGetUserById() {
+        given(userService.findUserById(same(1L)))
+                .willReturn(UserVo.builder().id("1").build());
+
+        var resp = controller.getUserById("1");
+        assertThat(resp.getStatusCode(), is(HttpStatus.OK));
+        assertThat(Objects.requireNonNull(resp.getBody()).getData(), allOf(
+                notNullValue(),
+                is(hasProperty("id", is("1")))
+        ));
+    }
+
+    @Test
+    public void testUpdateUserPwd() {
+        UserUpdatePasswordRequest request = new UserUpdatePasswordRequest();
+        request.setNewPwd("newPassword");
+        request.setCurrentUserPwd("correct");
+        var resp = controller.updateUserPwd("1", request);
+        assertThat(resp.getStatusCode(), is(HttpStatus.OK));
+
+        request.setCurrentUserPwd("wrong");
+        assertThrows(StarwhaleApiException.class,
+                () -> controller.updateUserPwd("1", request));
+    }
+
+    @Test
+    public void testUpdateUserState() {
+        given(userService.updateUserState(argThat(user -> user.getId() == 1L), same(true)))
+                .willReturn(true);
+
+        UserUpdateStateRequest request = new UserUpdateStateRequest();
+        request.setIsEnabled(true);
+
+        var resp = controller.updateUserState("1", request);
+        assertThat(resp.getStatusCode(), is(HttpStatus.OK));
+    }
+
+    @Test
+    public void testAddUserSystemRole() {
+        given(projectService.addProjectRole(same("0"), same(1L), same(1L)))
+                .willReturn(true);
+
+        UserRoleAddRequest request = new UserRoleAddRequest();
+        request.setCurrentUserPwd("correct");
+        request.setUserId("1");
+        request.setRoleId("1");
+
+        var resp = controller.addUserSystemRole(request);
+        assertThat(resp.getStatusCode(), is(HttpStatus.OK));
+
+        request.setCurrentUserPwd("wrong");
+        assertThrows(StarwhaleApiException.class,
+                () -> controller.addUserSystemRole(request));
+
+        request.setCurrentUserPwd("correct");
+        request.setUserId("2");
+        assertThrows(StarwhaleApiException.class,
+                () -> controller.addUserSystemRole(request));
+    }
+
+    @Test
+    public void testUpdateUserSystemRole() {
+        given(projectService.modifyProjectRole(same("0"), same(1L), same(1L)))
+                .willReturn(true);
+
+        UserRoleUpdateRequest request = new UserRoleUpdateRequest();
+        request.setRoleId("1");
+        request.setCurrentUserPwd("correct");
+        var resp = controller.updateUserSystemRole("1", request);
+        assertThat(resp.getStatusCode(), is(HttpStatus.OK));
+
+        assertThrows(StarwhaleApiException.class,
+                () -> controller.updateUserSystemRole("2", request));
+
+        request.setCurrentUserPwd("wrong");
+        assertThrows(StarwhaleApiException.class,
+                () -> controller.updateUserSystemRole("1", request));
+    }
+
+    @Test
+    public void testDeleteUserSystemRole() {
+        given(projectService.deleteProjectRole(same("0"), same(1L)))
+                .willReturn(true);
+
+        UserRoleDeleteRequest request = new UserRoleDeleteRequest();
+        request.setCurrentUserPwd("correct");
+        var resp = controller.deleteUserSystemRole("1", request);
+        assertThat(resp.getStatusCode(), is(HttpStatus.OK));
+
+        request.setCurrentUserPwd("wrong");
+        assertThrows(StarwhaleApiException.class,
+                () -> controller.deleteUserSystemRole("1", request));
+    }
+
+    @Test
+    public void testListRoles() {
+        given(userService.listRoles())
+                .willReturn(List.of(RoleVo.builder().id("1").build()));
+
+        var resp = controller.listRoles();
+        assertThat(resp.getStatusCode(), is(HttpStatus.OK));
+        assertThat(Objects.requireNonNull(resp.getBody()).getData(), allOf(
+                notNullValue(),
+                is(iterableWithSize(1)),
+                is(hasItem(hasProperty("id", is("1"))))
+        ));
+    }
+
+    @Test
+    public void testListSystemRoles() {
+        given(userService.listSystemRoles())
+                .willReturn(List.of(SystemRoleVo.builder().id("1").build()));
+
+        var resp = controller.listSystemRoles();
+        assertThat(resp.getStatusCode(), is(HttpStatus.OK));
+        assertThat(Objects.requireNonNull(resp.getBody()).getData(), allOf(
+                notNullValue(),
+                is(iterableWithSize(1)),
+                is(hasItem(hasProperty("id", is("1"))))
+        ));
+    }
+
+    @Test
+    public void testCheckCurrentUserPassword() {
+        UserCheckPasswordRequest request = new UserCheckPasswordRequest();
+        request.setCurrentUserPwd("correct");
+
+        var resp = controller.checkCurrentUserPassword(request);
+        assertThat(resp.getStatusCode(), is(HttpStatus.OK));
+
+        request.setCurrentUserPwd("wrong");
+        resp = controller.checkCurrentUserPassword(request);
+        assertThat(resp.getStatusCode(), is(HttpStatus.FORBIDDEN));
+    }
+
+    @Test
+    public void testUpdateCurrentUserPassword() {
+        UserUpdatePasswordRequest request = new UserUpdatePasswordRequest();
+        request.setCurrentUserPwd("correct");
+        request.setNewPwd("newPassword");
+
+        var resp = controller.updateCurrentUserPassword(request);
+        assertThat(resp.getStatusCode(), is(HttpStatus.OK));
+
+        request.setCurrentUserPwd("wrong");
+        assertThrows(StarwhaleApiException.class,
+                () -> controller.updateCurrentUserPassword(request));
+    }
+
+    @Test
+    public void testUserToken() {
+        given(userService.loadUserById(same(2L)))
+                .willReturn(User.builder().id(2L).name("test").build());
+        given(jwtTokenUtil.generateAccessToken(argThat(user -> user.getName().equals("test"))))
+                .willReturn("token");
+
+        var resp = controller.userToken(2L);
+        assertThat(resp.getStatusCode(), is(HttpStatus.OK));
+        assertThat(Objects.requireNonNull(resp.getBody()).getData(), is("token"));
+    }
+}
---
+++ server/controller/src/test/java/ai/starwhale/mlops/domain/user/UserServiceTest.java
@@ -0,0 +1,362 @@
+/*
+ * Copyright 2022 Starwhale, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ai.starwhale.mlops.domain.user;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.hasProperty;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.iterableWithSize;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.ArgumentMatchers.isNull;
+import static org.mockito.ArgumentMatchers.same;
+import static org.mockito.BDDMockito.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.mock;
+
+import ai.starwhale.mlops.api.protocol.user.RoleVo;
+import ai.starwhale.mlops.api.protocol.user.SystemRoleVo;
+import ai.starwhale.mlops.api.protocol.user.UserRoleVo;
+import ai.starwhale.mlops.api.protocol.user.UserVo;
+import ai.starwhale.mlops.common.PageParams;
+import ai.starwhale.mlops.configuration.security.SwPasswordEncoder;
+import ai.starwhale.mlops.domain.project.ProjectManager;
+import ai.starwhale.mlops.domain.project.mapper.ProjectMapper;
+import ai.starwhale.mlops.domain.project.mapper.ProjectRoleMapper;
+import ai.starwhale.mlops.domain.project.po.ProjectEntity;
+import ai.starwhale.mlops.domain.project.po.ProjectRoleEntity;
+import ai.starwhale.mlops.domain.user.bo.User;
+import ai.starwhale.mlops.domain.user.mapper.RoleMapper;
+import ai.starwhale.mlops.domain.user.mapper.UserMapper;
+import ai.starwhale.mlops.domain.user.po.RoleEntity;
+import ai.starwhale.mlops.domain.user.po.UserEntity;
+import ai.starwhale.mlops.exception.api.StarwhaleApiException;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.context.SecurityContextImpl;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserServiceTest {
+
+    private UserService service;
+    private UserMapper userMapper;
+    private RoleMapper roleMapper;
+    private ProjectMapper projectMapper;
+    private ProjectRoleMapper projectRoleMapper;
+    private ProjectManager projectManager;
+
+    @BeforeEach
+    public void setUp() {
+        userMapper = mock(UserMapper.class);
+        UserEntity u1 = UserEntity.builder()
+                .id(1L)
+                .userEnabled(1)
+                .userName("user1")
+                .userPwd("password1")
+                .userPwdSalt("salt1")
+                .build();
+        given(userMapper.findUserByName(same("user1"))).willReturn(u1);
+        given(userMapper.findUser(same(1L))).willReturn(u1);
+        given(userMapper.findUserByName(same("current")))
+                .willReturn(UserEntity.builder()
+                        .id(1L)
+                        .userName("current")
+                        .userEnabled(1)
+                        .userPwd(SwPasswordEncoder
+                                .getEncoder("current_salt")
+                                .encode("current_password"))
+                        .userPwdSalt("current_salt")
+                        .build());
+        roleMapper = mock(RoleMapper.class);
+        projectMapper = mock(ProjectMapper.class);
+        projectRoleMapper = mock(ProjectRoleMapper.class);
+        projectManager = mock(ProjectManager.class);
+        given(projectManager.getProjectId(same("0"))).willReturn(0L);
+        given(projectManager.getProjectId(same("1"))).willReturn(1L);
+        given(projectManager.getProjectId(same("2"))).willReturn(2L);
+        given(projectManager.getProjectId(same("3"))).willReturn(3L);
+        UserConvertor userConvertor = mock(UserConvertor.class);
+        given(userConvertor.convert(any(UserEntity.class)))
+                .willAnswer(invocation -> {
+                    UserEntity entity = invocation.getArgument(0);
+                    return UserVo.builder()
+                            .id(String.valueOf(entity.getId()))
+                            .name(entity.getUserName())
+                            .isEnabled(entity.getUserEnabled() != null && entity.getUserEnabled() == 1)
+                            .build();
+                });
+        RoleConvertor roleConvertor = mock(RoleConvertor.class);
+        given(roleConvertor.convert(any(RoleEntity.class)))
+                .willReturn(RoleVo.empty());
+        UserRoleConvertor userRoleConvertor = mock(UserRoleConvertor.class);
+        given(userRoleConvertor.convert(any(ProjectRoleEntity.class)))
+                .willReturn(UserRoleVo.builder().build());
+        SystemRoleConvertor systemRoleConvertor = mock(SystemRoleConvertor.class);
+        given(systemRoleConvertor.convert(any(ProjectRoleEntity.class)))
+                .willReturn(SystemRoleVo.builder().build());
+        SaltGenerator saltGenerator = mock(SaltGenerator.class);
+        given(saltGenerator.salt()).willReturn("salt");
+        service = new UserService(userMapper, roleMapper, projectMapper, projectRoleMapper, projectManager,
+                userConvertor, roleConvertor, userRoleConvertor, systemRoleConvertor, saltGenerator);
+
+        User current = User.builder().id(1L).name("current").active(true).build();
+        var token = new UsernamePasswordAuthenticationToken(current, null);
+
+        SecurityContext context = new SecurityContextImpl(token);
+
+        SecurityContextHolder.setContext(context);
+    }
+
+    @Test
+    public void testCurrentUserDetail() {
+        var res = service.currentUserDetail();
+        assertThat(res, allOf(
+               notNullValue(),
+               hasProperty("id", is(1L)),
+               hasProperty("name", is("current"))
+        ));
+        SecurityContextHolder.clearContext();
+        assertThrows(StarwhaleApiException.class,
+                () -> service.currentUserDetail());
+    }
+
+    @Test
+    public void testCurrentUser() {
+        var res = service.currentUser();
+        assertThat(res, allOf(
+                notNullValue(),
+                hasProperty("id", is("1")),
+                hasProperty("name", is("current")),
+                hasProperty("isEnabled", is(true))
+        ));
+    }
+
+    @Test
+    public void testLoadUserByUsername() {
+        var res = service.loadUserByUsername("user1");
+        assertThat(res, allOf(
+                notNullValue(),
+                is(hasProperty("id", is(1L))),
+                is(hasProperty("name", is("user1"))),
+                is(hasProperty("password", is("password1"))),
+                is(hasProperty("salt", is("salt1"))),
+                is(hasProperty("active", is(true)))
+        ));
+
+        assertThrows(UsernameNotFoundException.class,
+                () -> service.loadUserByUsername("wrong"));
+    }
+
+    @Test
+    public void testCheckCurrentUserPassword() {
+        var res = service.checkCurrentUserPassword("current_password");
+        assertThat(res, is(true));
+
+        res = service.checkCurrentUserPassword("wrong_password");
+        assertThat(res, is(false));
+    }
+
+    @Test
+    public void testLoadUserById() {
+        var res = service.loadUserById(1L);
+        assertThat(res, allOf(
+                notNullValue(),
+                is(hasProperty("id", is(1L))),
+                is(hasProperty("name", is("user1"))),
+                is(hasProperty("password", is("password1"))),
+                is(hasProperty("salt", is("salt1"))),
+                is(hasProperty("active", is(true)))
+        ));
+
+        assertThrows(UsernameNotFoundException.class,
+                () -> service.loadUserById(2L));
+    }
+
+    @Test
+    public void testGetProjectRolesOfUser() {
+        RoleEntity owner = RoleEntity.builder().id(1L).roleName("Owner").build();
+        RoleEntity guest = RoleEntity.builder().id(3L).roleName("Guest").build();
+        given(roleMapper.getRolesOfProject(same(1L), same(1L)))
+                .willReturn(List.of(owner));
+        given(roleMapper.getRolesOfProject(same(2L), same(2L)))
+                .willReturn(List.of(guest));
+        given(projectMapper.findProject(same(3L)))
+                .willReturn(ProjectEntity.builder().id(3L).privacy(1).build());
+
+
+        var res = service.getProjectRolesOfUser(User.builder().id(1L).build(), "1");
+        assertThat(res, allOf(
+                notNullValue(),
+                is(iterableWithSize(1)),
+                is(hasItem(hasProperty("roleName", is("Owner"))))
+        ));
+
+        res = service.getProjectRolesOfUser(User.builder().id(2L).build(), "2");
+        assertThat(res, allOf(
+                notNullValue(),
+                is(iterableWithSize(1)),
+                is(hasItem(hasProperty("roleName", is("Guest"))))
+        ));
+
+        res = service.getProjectRolesOfUser(User.builder().id(2L).build(), "3");
+        assertThat(res, allOf(
+                notNullValue(),
+                is(iterableWithSize(1)),
+                is(hasItem(hasProperty("roleName", is("Guest"))))
+        ));
+    }
+
+    @Test
+    public void findUserById() {
+        var res = service.findUserById(1L);
+        assertThat(res, allOf(
+                notNullValue(),
+                hasProperty("name", is("user1"))
+        ));
+    }
+
+    @Test
+    public void testListUser() {
+        UserEntity u1 = UserEntity.builder().id(1L).build();
+        UserEntity u2 = UserEntity.builder().id(2L).build();
+        UserEntity u3 = UserEntity.builder().id(3L).build();
+        given(userMapper.listUsers(any()))
+                .willReturn(List.of(u1, u2, u3));
+        given(userMapper.listUsers(same("u1")))
+                .willReturn(List.of(u1));
+
+        var res = service.listUsers(User.builder().build(), new PageParams(1, 10));
+        assertThat(res, allOf(
+                notNullValue(),
+                is(hasProperty("total", is(3L))),
+                is(hasProperty("list", is(iterableWithSize(3))))
+        ));
+        System.out.println(res);
+
+        res = service.listUsers(User.builder().name("u1").build(), new PageParams(1, 10));
+        assertThat(res, allOf(
+                notNullValue(),
+                is(hasProperty("total", is(1L))),
+                is(hasProperty("list", is(iterableWithSize(1)))),
+                is(hasProperty("list", is(hasItem(hasProperty("id", is("1"))))))
+        ));
+    }
+
+    @Test
+    public void testCreateUser() {
+        given(userMapper.createUser(any(UserEntity.class)))
+                .willAnswer(invocation -> {
+                   var entity = (UserEntity)invocation.getArgument(0);
+                   entity.setId(1L);
+                   return null;
+                });
+
+        var res = service.createUser(User.builder().name("test").build(), "password", "salt");
+        assertThat(res, is(1L));
+
+        assertThrows(StarwhaleApiException.class,
+                () -> service.createUser(User.builder().name("user1").build(), "password", "salt"));
+    }
+
+    @Test
+    public void testChangePassword() {
+        given(userMapper.changePassword(argThat(user -> user.getId() == 1L)))
+                .willReturn(1);
+
+        var res = service.changePassword(User.builder().id(1L).build(), "new", "old");
+        assertThat(res, is(true));
+
+        res = service.changePassword(User.builder().id(2L).build(), "new");
+        assertThat(res, is(false));
+    }
+
+    @Test
+    public void testUpdateUserState() {
+        given(userMapper.enableUser(argThat(user -> user.getId() == 1L)))
+                .willReturn(1);
+
+        var res = service.updateUserState(User.builder().id(1L).build(), true);
+        assertThat(res, is(true));
+
+        res = service.updateUserState(User.builder().id(2L).build(), true);
+        assertThat(res, is(false));
+    }
+
+    @Test
+    public void testListSystemRoles() {
+        given(projectRoleMapper.listSystemRoles())
+                .willReturn(List.of(ProjectRoleEntity.builder().id(1L).build(),
+                        ProjectRoleEntity.builder().id(2L).build()));
+
+        var res = service.listSystemRoles();
+        assertThat(res, allOf(
+                notNullValue(),
+                is(iterableWithSize(2))
+        ));
+    }
+
+    @Test
+    public void testListRoles() {
+        given(roleMapper.listRoles())
+                .willReturn(List.of(RoleEntity.builder().id(1L).build(), RoleEntity.builder().id(2L).build()));
+
+        var res = service.listRoles();
+        assertThat(res, allOf(
+                notNullValue(),
+                is(iterableWithSize(2))
+        ));
+    }
+
+    @Test
+    public void testListUserRoles() {
+        given(projectRoleMapper.listUserRoles(same(1L), isNull()))
+                .willReturn(List.of(
+                        ProjectRoleEntity.builder().id(1L).build(),
+                        ProjectRoleEntity.builder().id(2L).build()
+                ));
+        given(projectRoleMapper.listUserRoles(same(1L), same(1L)))
+                .willReturn(List.of(
+                        ProjectRoleEntity.builder().id(1L).build()
+                ));
+
+        var res = service.listUserRoles(1L, "1");
+        assertThat(res, allOf(
+                notNullValue(),
+                is(iterableWithSize(1))
+        ));
+
+        res = service.listUserRoles(1L, "");
+        assertThat(res, allOf(
+                notNullValue(),
+                is(iterableWithSize(2))
+        ));
+
+        res = service.listCurrentUserRoles("1");
+        assertThat(res, allOf(
+                notNullValue(),
+                is(iterableWithSize(1))
+        ));
+
+    }
+}
