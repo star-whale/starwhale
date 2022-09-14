59600ebc1c811619579f7e081b9ffffdc9aaa413
liuyunxi <205407@qq.com>
2022-09-14 12:05:46 +0800

    unit test for methods of version order

---
+++ server/controller/src/test/java/ai/starwhale/mlops/domain/bundle/BundleManagerTest.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright 2022 Starwhale, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ai.starwhale.mlops.domain.bundle;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.same;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+
+import ai.starwhale.mlops.common.IdConvertor;
+import ai.starwhale.mlops.common.VersionAliasConvertor;
+import ai.starwhale.mlops.domain.bundle.base.BundleVersionEntity;
+import ai.starwhale.mlops.domain.project.ProjectAccessor;
+import ai.starwhale.mlops.exception.SwValidationException.ValidSubject;
+import ai.starwhale.mlops.exception.api.StarwhaleApiException;
+import lombok.Builder;
+import lombok.Data;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class BundleManagerTest {
+
+    private BundleManager bundleManager;
+    private ProjectAccessor projectAccessor;
+    private BundleAccessor bundleAccessor;
+    private BundleVersionAccessor bundleVersionAccessor;
+
+    @BeforeEach
+    public void setUp() {
+        projectAccessor = mock(ProjectAccessor.class);
+        bundleAccessor = mock(BundleAccessor.class);
+        bundleVersionAccessor = mock(BundleVersionAccessor.class);
+        bundleManager = new BundleManager(
+                new IdConvertor(),
+                new VersionAliasConvertor(),
+                projectAccessor,
+                bundleAccessor,
+                bundleVersionAccessor,
+                ValidSubject.PROJECT
+        );
+    }
+
+    @Test
+    public void testGetBundleVersionId() {
+        given(bundleVersionAccessor.findVersionById(same(1L)))
+                .willReturn(EntityWrapper.builder().id(1L).name("byId").build());
+        given(bundleVersionAccessor.findVersionByAliasAndBundleId(anyString(), anyLong()))
+                .willReturn(EntityWrapper.builder().id(2L).name("byAlias").build());
+        given(bundleVersionAccessor.findVersionByNameAndBundleId(anyString(), anyLong()))
+                .willReturn(EntityWrapper.builder().id(3L).name("byName").build());
+
+        var res = bundleManager.getBundleVersionId(
+                BundleVersionUrl.create("", "", "1"), 1L);
+        assertThat(res, is(1L));
+
+        res = bundleManager.getBundleVersionId(
+                BundleVersionUrl.create("", "", "v1"), 1L);
+        assertThat(res, is(2L));
+
+        res = bundleManager.getBundleVersionId(
+                BundleVersionUrl.create("", "", "vName"), 1L);
+        assertThat(res, is(3L));
+
+        assertThrows(StarwhaleApiException.class,
+                () -> bundleManager.getBundleVersionId(BundleVersionUrl.create("", "", "2"), 1L));
+
+
+    }
+
+    @Data
+    @Builder
+    private static class EntityWrapper implements BundleVersionEntity {
+
+        private Long id;
+        private String name;
+
+        @Override
+        public Long getId() {
+            return id;
+        }
+
+        @Override
+        public String getName() {
+            return name;
+        }
+    }
+}
---
+++ server/controller/src/test/java/ai/starwhale/mlops/domain/runtime/RuntimeManagerTest.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2022 Starwhale, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ai.starwhale.mlops.domain.runtime;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.hasProperty;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.isA;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+
+import ai.starwhale.mlops.common.IdConvertor;
+import ai.starwhale.mlops.common.VersionAliasConvertor;
+import ai.starwhale.mlops.domain.runtime.mapper.RuntimeMapper;
+import ai.starwhale.mlops.domain.runtime.mapper.RuntimeVersionMapper;
+import ai.starwhale.mlops.domain.runtime.po.RuntimeVersionEntity;
+import ai.starwhale.mlops.domain.swmp.po.SwModelPackageVersionEntity;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class RuntimeManagerTest {
+
+    private RuntimeManager manager;
+    private RuntimeMapper runtimeMapper;
+    private RuntimeVersionMapper versionMapper;
+
+    @BeforeEach
+    public void setUp() {
+        runtimeMapper = mock(RuntimeMapper.class);
+        versionMapper = mock(RuntimeVersionMapper.class);
+        manager = new RuntimeManager(
+                runtimeMapper,
+                versionMapper,
+                new IdConvertor(),
+                new VersionAliasConvertor()
+        );
+    }
+
+    @Test
+    public void testFindVersionByAliasAndBundleId() {
+        given(versionMapper.findByVersionOrderAndRuntimeId(anyLong(), anyLong()))
+                .willReturn(RuntimeVersionEntity.builder()
+                        .id(1L)
+                        .versionName("runtime1")
+                        .build());
+        var res = manager.findVersionByAliasAndBundleId("v1", 1L);
+        assertThat(res, allOf(
+                notNullValue(),
+                isA(SwModelPackageVersionEntity.class),
+                hasProperty("id", is(1L)),
+                hasProperty("versionName", is("runtime1"))
+        ));
+    }
+}
