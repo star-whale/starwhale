e03184e88724e092bd88cb72181989e5873d227b
gaoxinxing <15031259256@163.com>
2022-09-14 20:05:09 +0800

    init model test code

--- scripts/client_test/cmds/model_cmd.py
+++ scripts/client_test/cmds/model_cmd.py
@@ -1,35 +1,38 @@
 from typing import Tuple
 
-from base.invoke import invoke
+from .base.invoke import invoke
+from .base.environment import CLI
 
 
 class Model:
+    model_cmd = "model"
+
     def build(self) -> Tuple[str, str]:
-        return invoke(["", "", "", "", "", "", "", ])
+        return invoke([CLI, self.model_cmd, "build", "", "", "", "", "", "", ])
 
     def info(self) -> Tuple[str, str]:
-        return invoke(["", "", "", "", "", "", "", ])
+        return invoke([CLI, self.model_cmd, "info", "", "", "", "", "", "", ])
 
     def list(self) -> Tuple[str, str]:
-        return invoke(["", "", "", "", "", "", "", ])
+        return invoke([CLI, self.model_cmd, "list", "", "", "", "", "", "", ])
 
     def eval(self) -> Tuple[str, str]:
-        return invoke(["", "", "", "", "", "", "", ])
+        return invoke([CLI, self.model_cmd, "eval", "", "", "", "", "", "", ])
 
     def copy(self) -> Tuple[str, str]:
-        return invoke(["", "", "", "", "", "", "", ])
+        return invoke([CLI, self.model_cmd, "copy", "", "", "", "", "", "", ])
 
     def extract(self) -> Tuple[str, str]:
-        return invoke(["", "", "", "", "", "", "", ])
+        return invoke([CLI, self.model_cmd, "extract", "", "", "", "", "", "", ])
 
     def remove(self) -> Tuple[str, str]:
-        return invoke(["", "", "", "", "", "", "", ])
+        return invoke([CLI, self.model_cmd, "remove", "", "", "", "", "", "", ])
 
     def recover(self) -> Tuple[str, str]:
-        return invoke(["", "", "", "", "", "", "", ])
+        return invoke([CLI, self.model_cmd, "recover", "", "", "", "", "", "", ])
 
     def history(self) -> Tuple[str, str]:
-        return invoke(["", "", "", "", "", "", "", ])
+        return invoke([CLI, self.model_cmd, "history", "", "", "", "", "", "", ])
 
     def tag(self) -> Tuple[str, str]:
-        return invoke(["", "", "", "", "", "", "", ])
+        return invoke([CLI, self.model_cmd, "tag", "", "", "", "", "", "", ])
