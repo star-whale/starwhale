68cc9fe7cf0e07897c059a5996bc8b857425efca
tianwei <liutianweidlut@gmail.com>
2022-09-15 10:20:31 +0800

    example: refactor nmt example (#1196)
    refactor nmt example

--- example/nmt/nmt/dataset.py
+++ example/nmt/nmt/dataset.py
@@ -3,20 +3,20 @@ from pathlib import Path
 
 from starwhale.api.dataset import Text, BuildExecutor
 
-from .helper import normalizeString
+from .helper import normalize_str
 
 
-class DataSetProcessExecutor(BuildExecutor):
+class NMTDatasetBuildExecutor(BuildExecutor):
     def iter_item(self) -> t.Generator[t.Tuple[t.Any, t.Any], None, None]:
         root_dir = Path(__file__).parent.parent / "data"
 
-        with (root_dir / "test_eng-fra.txt").open("r") as f:
+        with (root_dir / "fra-test.txt").open("r") as f:
             for line in f.readlines():
                 line = line.strip()
                 if not line or line.startswith("CC-BY"):
                     continue
 
-                _data, _label = line.split("\t", 1)
-                data = Text(normalizeString(_data), encoding="utf-8")
-                annotations = {"label": normalizeString(_label)}
+                _data, _label, *_ = line.split("\t")
+                data = Text(normalize_str(_data), encoding="utf-8")
+                annotations = {"label": normalize_str(_label)}
                 yield data, annotations
---
+++ example/nmt/nmt/ppl.py
@@ -0,0 +1,89 @@
+import os
+
+import torch
+
+from starwhale.api.job import Context
+from starwhale.api.model import PipelineHandler
+from starwhale.api.dataset import Text
+
+from .bleu import calculate_bleu
+from .model import DecoderRNN, EncoderRNN
+from .helper import EOS_token, SOS_token, MAX_LENGTH, sentence_to_tensor
+
+_ROOT_DIR = os.path.dirname(os.path.dirname(__file__))
+
+
+class NMTPipeline(PipelineHandler):
+    def __init__(self, context: Context) -> None:
+        super().__init__(context=context)
+
+        self.hidden_size = 256
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.vocab = self._load_vocab()
+        self.encoder = self._load_encoder_model(self.device)
+        self.decoder = self._load_decoder_model(self.device)
+        self.max_length = MAX_LENGTH
+
+    @torch.no_grad()
+    def ppl(self, text: Text, **kw):
+        input_tensor = sentence_to_tensor(self.vocab.vin, text.content, self.device)
+        input_length = input_tensor.size()[0]
+
+        encoder_hidden = self.encoder.initHidden()
+        encoder_outputs = torch.zeros(
+            self.max_length, self.encoder.hidden_size, device=self.device
+        )
+        for ei in range(input_length):
+            encoder_output, encoder_hidden = self.encoder(
+                input_tensor[ei], encoder_hidden
+            )
+            encoder_outputs[ei] += encoder_output[0, 0]
+
+        decoder_input = torch.tensor([[SOS_token]], device=self.device)
+        decoder_hidden = encoder_hidden
+        decoded_words = []
+        decoder_attentions = torch.zeros(self.max_length, self.max_length)
+
+        for di in range(self.max_length):
+            decoder_output, decoder_hidden, decoder_attention = self.decoder(
+                decoder_input, decoder_hidden, encoder_outputs
+            )
+            decoder_attentions[di] = decoder_attention.data
+            _, topi = decoder_output.data.topk(1)
+            if topi.item() == EOS_token:
+                break
+            else:
+                decoded_words.append(self.vocab.vout.index2word[topi.item()])
+            decoder_input = topi.squeeze().detach()
+
+        return " ".join(decoded_words)
+
+    def cmp(self, _data_loader):
+        result, label = [], []
+        for _data in _data_loader:
+            result.append(_data["result"])
+            label.append(_data["annotations"]["label"])
+
+        bleu = calculate_bleu(result, [label])
+        print(f"bleu: {bleu}")
+        report = {"bleu_score": bleu}
+        self.evaluation.log_metrics(report)
+
+    def _load_vocab(self):
+        # hack for torch load
+        from .helper import Lang, Vocab  # type: ignore
+
+        return torch.load(_ROOT_DIR + "/models/vocab_eng-fra.bin")
+
+    def _load_encoder_model(self, device):
+        model = EncoderRNN(self.vocab.vin.n_words, self.hidden_size, device).to(device)
+
+        param = torch.load(_ROOT_DIR + "/models/encoder.pth", device)
+        model.load_state_dict(param)
+        return model
+
+    def _load_decoder_model(self, device):
+        model = DecoderRNN(self.vocab.vout.n_words, self.hidden_size, device).to(device)
+        param = torch.load(_ROOT_DIR + "/models/decoder.pth", device)
+        model.load_state_dict(param)
+        return model
