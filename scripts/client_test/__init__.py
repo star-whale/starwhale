e09ed3be2b9687013d3f3864e91dfdd11a99f802
gaoxinxing <15031259256@163.com>
2022-09-16 16:46:18 +0800

    add cloud test flow

--- scripts/client_test/__init__.py
+++ scripts/client_test/__init__.py
@@ -1,3 +1,5 @@
 import os
 
-ROOT_DIR = os.path.dirname(__file__)
+CURRENT_DIR = os.path.dirname(__file__)
+SCRIPT_DIR = os.path.abspath(os.path.join(CURRENT_DIR, os.pardir))
+ROOT_DIR = os.path.abspath(os.path.join(SCRIPT_DIR, os.pardir))
--- scripts/client_test/cmds/base/environment.py
+++ scripts/client_test/cmds/base/environment.py
@@ -1,16 +1,15 @@
 import os
-import tempfile
 
 from .invoke import invoke
 
 
-
 class Environment:
-    def __init__(self, src_dir: str):
+    def __init__(self, src_dir: str, work_dir: str):
         self.src_dir = src_dir
-        self.tmp_dir = tempfile.TemporaryDirectory()
-        self.work_dir: str = self.tmp_dir.name
-        self._init_local_data_dir()
+        self.work_dir = work_dir
+
+    def prepare(self):
+        self._config_local_data_dir()
         self._install_sw()
         self._create_test_dir()
         self._download_and_extract_test_data()
@@ -20,12 +19,14 @@ class Environment:
 
         invoke(["pip", "install", "-e", f"{self.work_dir}/client"])
 
-    def _init_local_data_dir(self):
+    def _config_local_data_dir(self):
         os.environ["SW_CLI_CONFIG"] = f"{self.work_dir}/config.yaml"
         os.environ["SW_LOCAL_STORAGE"] = f"{self.work_dir}/data"
 
     def _create_test_dir(self):
         invoke(["cp", "-rf", f"{self.src_dir}/example", f"{self.work_dir}/example"])
+        invoke(["rm", "-rf", f"{self.work_dir}/example/mnist/.venv"])
+        invoke(["rm", "-rf", f"{self.work_dir}/example/mnist/runtime.yaml"])
 
     def _download_and_extract_test_data(self):
         # TODO use make
@@ -41,6 +42,3 @@ class Environment:
         invoke(["gzip", "-d", f"{self.work_dir}/example/mnist/data/train-labels-idx1-ubyte.gz"])
         invoke(["gzip", "-d", f"{self.work_dir}/example/mnist/data/t10k-images-idx3-ubyte.gz"])
         invoke(["gzip", "-d", f"{self.work_dir}/example/mnist/data/t10k-labels-idx1-ubyte.gz"])
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self.tmp_dir.cleanup()
