f62fac92370ffd82a220ab558a343ab3042e4cf5
Jialei <3217223+jialeicui@users.noreply.github.com>
2022-09-19 13:44:23 +0800

    fix(client): fix dataset size info in cloud (#1237)
    fix(cli): fix dataset size info in cloud

---
+++ client/tests/base/test_cloud.py
@@ -0,0 +1,27 @@
+from unittest import TestCase
+
+import yaml
+
+from starwhale.base.cloud import CloudRequestMixed
+
+
+class TestCloudRequestMixed(TestCase):
+    def test_get_bundle_size_from_resp(self):
+        ins = CloudRequestMixed()
+
+        item = {"size": 7}
+        size = ins.get_bundle_size_from_resp("whatever", item)
+        assert size == 7
+
+        meta = {"dataset_byte_size": 8}
+        item = {"meta": yaml.safe_dump(meta)}
+        size = ins.get_bundle_size_from_resp("dataset", item)
+        assert size == 8
+
+        item = {"no meta": ""}
+        size = ins.get_bundle_size_from_resp("dataset", item)
+        assert size == 0
+
+        item = {"meta": "no dataset byte size"}
+        size = ins.get_bundle_size_from_resp("dataset", item)
+        assert size == 0
