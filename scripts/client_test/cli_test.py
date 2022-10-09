684ca743e695d30c0ad6a6be4d8c8f23580abf55
anda-ren <781905270@qq.com>
2022-10-09 15:59:41 +0800

    fix e2e error

--- scripts/client_test/cli_test.py
+++ scripts/client_test/cli_test.py
@@ -118,7 +118,7 @@ class TestCli:
             dataset=swds["version"],
             runtime=swrt["version"],
             project=cloud_project,
-            step_spec=f"{CURRENT_DIR}/step_spec.yaml",
+            step_spec=f"{os.path.abspath(CURRENT_DIR)}/step_spec.yaml",
         )
         _new_job_list = self.evaluation.list(project=cloud_project)
         assert len(_new_job_list) == len(_origin_job_list) + 1
