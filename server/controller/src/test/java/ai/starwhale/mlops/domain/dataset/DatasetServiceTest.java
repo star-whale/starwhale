f1efba90df2fce44c36207766f0208bed2b5b92b
liuyunxi <205407@qq.com>
2022-11-03 18:06:57 +0800

    unit test for trash api

--- server/controller/src/test/java/ai/starwhale/mlops/domain/dataset/DatasetServiceTest.java
+++ server/controller/src/test/java/ai/starwhale/mlops/domain/dataset/DatasetServiceTest.java
@@ -42,7 +42,6 @@ import ai.starwhale.mlops.common.VersionAliasConvertor;
 import ai.starwhale.mlops.domain.bundle.BundleManager;
 import ai.starwhale.mlops.domain.bundle.BundleUrl;
 import ai.starwhale.mlops.domain.bundle.BundleVersionUrl;
-import ai.starwhale.mlops.domain.bundle.recover.RecoverManager;
 import ai.starwhale.mlops.domain.bundle.remove.RemoveManager;
 import ai.starwhale.mlops.domain.bundle.revert.RevertManager;
 import ai.starwhale.mlops.domain.dataset.bo.DatasetQuery;
@@ -58,6 +57,7 @@ import ai.starwhale.mlops.domain.dataset.po.DatasetVersionEntity;
 import ai.starwhale.mlops.domain.project.ProjectManager;
 import ai.starwhale.mlops.domain.project.po.ProjectEntity;
 import ai.starwhale.mlops.domain.storage.StorageService;
+import ai.starwhale.mlops.domain.trash.TrashService;
 import ai.starwhale.mlops.domain.user.UserService;
 import ai.starwhale.mlops.domain.user.bo.User;
 import ai.starwhale.mlops.exception.SwValidationException;
@@ -82,6 +82,8 @@ public class DatasetServiceTest {
     private DatasetManager datasetManager;
     private UserService userService;
     private DsFileGetter dsFileGetter;
+
+    private TrashService trashService;
     @Setter
     private BundleManager bundleManager;
 
@@ -127,6 +129,8 @@ public class DatasetServiceTest {
 
         dsFileGetter = mock(DsFileGetter.class);
 
+        trashService = mock(TrashService.class);
+
         service = new DatasetService(
                 projectManager,
                 datasetMapper,
@@ -138,8 +142,8 @@ public class DatasetServiceTest {
                 new IdConvertor(),
                 new VersionAliasConvertor(),
                 userService,
-                dsFileGetter
-        );
+                dsFileGetter,
+                trashService);
         bundleManager = mock(BundleManager.class);
         given(bundleManager.getBundleId(any(BundleUrl.class)))
                 .willAnswer(invocation -> {
@@ -183,33 +187,15 @@ public class DatasetServiceTest {
     public void testDeleteDataset() {
         RemoveManager removeManager = mock(RemoveManager.class);
         given(removeManager.removeBundle(argThat(
-                url -> Objects.equals(url.getProjectUrl(), "p1") && Objects.equals(url.getBundleUrl(), "d1")
+                url -> Objects.equals(url.getProjectUrl(), "p1") && Objects.equals(url.getBundleUrl(), "1")
         ))).willReturn(true);
         try (var mock = mockStatic(RemoveManager.class)) {
             mock.when(() -> RemoveManager.create(any(), any()))
                     .thenReturn(removeManager);
-            var res = service.deleteDataset(DatasetQuery.builder().projectUrl("p1").datasetUrl("d1").build());
+            var res = service.deleteDataset(DatasetQuery.builder().projectUrl("p1").datasetUrl("1").build());
             assertThat(res, is(true));
 
-            res = service.deleteDataset(DatasetQuery.builder().projectUrl("p2").datasetUrl("d2").build());
-            assertThat(res, is(false));
-        }
-    }
-
-    @Test
-    public void testRecoverDataset() {
-        RecoverManager recoverManager = mock(RecoverManager.class);
-        given(recoverManager.recoverBundle(argThat(
-                url -> Objects.equals(url.getProjectUrl(), "p1") && Objects.equals(url.getBundleUrl(), "d1")
-        ))).willReturn(true);
-        try (var mock = mockStatic(RecoverManager.class)) {
-            mock.when(() -> RecoverManager.create(any(), any(), any()))
-                    .thenReturn(recoverManager);
-
-            var res = service.recoverDataset("p1", "d1");
-            assertThat(res, is(true));
-
-            res = service.recoverDataset("p1", "d2");
+            res = service.deleteDataset(DatasetQuery.builder().projectUrl("p2").datasetUrl("2").build());
             assertThat(res, is(false));
         }
     }
