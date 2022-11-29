b113c49638c3b7cb8b20b87e79963a5f3b75547b
anda-ren <renyanda@starwhale.ai>
2022-11-29 18:09:23 +0800

    fix ut error

--- server/controller/src/test/java/ai/starwhale/mlops/domain/task/log/TaskLogK8sCollectorTest.java
+++ server/controller/src/test/java/ai/starwhale/mlops/domain/task/log/TaskLogK8sCollectorTest.java
@@ -18,6 +18,7 @@ package ai.starwhale.mlops.domain.task.log;
 
 import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -29,6 +30,8 @@ import ai.starwhale.mlops.schedule.k8s.K8sClient;
 import ai.starwhale.mlops.schedule.k8s.K8sJobTemplate;
 import ai.starwhale.mlops.storage.StorageAccessService;
 import io.kubernetes.client.openapi.ApiException;
+import io.kubernetes.client.openapi.models.V1ObjectMeta;
+import io.kubernetes.client.openapi.models.V1Pod;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import org.junit.jupiter.api.BeforeEach;
@@ -55,9 +58,11 @@ public class TaskLogK8sCollectorTest {
     @Test
     public void testNormal() throws IOException, ApiException {
         String log = "this is log";
-        when(k8sClient.logOfJob(anyString(), anyList())).thenReturn(log);
+        V1Pod v1Pod = new V1Pod().metadata(new V1ObjectMeta().name("x"));
+        when(k8sClient.podOfJob(anyString())).thenReturn(v1Pod);
+        when(k8sClient.logOfPod(eq(v1Pod), anyList())).thenReturn(log);
         taskLogK8sCollector.collect(Task.builder().id(1L).resultRootPath(new ResultPath("root")).build());
-        verify(storageService).put("root/logs/log", log.getBytes(StandardCharsets.UTF_8));
+        verify(storageService).put(anyString(), eq(log.getBytes(StandardCharsets.UTF_8)));
 
     }
 
