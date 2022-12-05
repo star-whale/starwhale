5349b7e1c4f9da9ef42559992fd45f40cfc93f44
tianwei <liutianweidlut@gmail.com>
2022-12-05 22:37:55 +0800

    skip ut for macos && python3.11

--- client/setup.py
+++ client/setup.py
@@ -22,7 +22,8 @@ install_requires = [
     "commonmark>=0.9.1",
     "textual==0.1.18",
     "jsonlines==3.0.0",
-    "boto3==1.26.22",
+    # botocore(>=1.29.14) fixed the cgi deprecation warning which has been updated in the boto3 1.26.14 version
+    "boto3>=1.26.14",
     "scikit-learn>=0.20.0",
     # Python 3.11 needs dill >= 0.3.6
     "dill>=0.3.6",
