1d0c19a8f2aef5073849f1e73376089947ba8be7
anda-ren <renyanda@starwhale.ai>
2022-12-16 16:39:32 +0800

    add culane dataset

---
+++ example/datasets/culane/dataset.py
@@ -0,0 +1,55 @@
+import requests
+import json
+from starwhale import dataset, Link, Image, MIMEType  # noqa: F401
+
+PATH_ROOT = "https://starwhale-examples.oss-cn-beijing.aliyuncs.com/dataset/culane"
+DATA_PATH = "driver_182_30frame"
+SEG_LABEL_PATH = f"laneseg_label_w16/{DATA_PATH}"
+
+
+def do_iter_item():
+    ds = dataset("culane", create=True)
+    response = requests.get(f"{PATH_ROOT}/{DATA_PATH}/tree.json")
+    tree = json.loads(response.text)
+    for dir in tree["children"]:
+        dir_name_: str = dir["name"]
+        if not dir_name_.endswith(".MP4") or dir["type"] != "directory":
+            continue
+        files = dir["children"]
+        for f in files:
+            f_name_: str = f["name"]
+            if not f_name_.endswith(".jpg"):
+                continue
+            name_no_suff = f_name_.split(".")[0]
+            splitlines = []
+            while True:
+                try:
+                    splitlines = requests.get(
+                        f"{PATH_ROOT}/{DATA_PATH}/{dir_name_}/{name_no_suff}.lines.txt").text.splitlines()
+                    break
+                except Exception:
+                    print(f"timeout {name_no_suff}")
+                    continue
+
+            annotation = {
+                "mask": Link(
+                    with_local_fs_data=False,
+                    data_type=Image(
+                        display_name=f"{name_no_suff}.png", mime_type=MIMEType.PNG
+                    ),
+                    uri=f"{PATH_ROOT}/{SEG_LABEL_PATH}/{dir_name_}/{name_no_suff}.png",
+                ), "lines": splitlines
+            }
+            ds.append((f"{dir_name_}/{name_no_suff}", Link(
+                uri=f"{PATH_ROOT}/{DATA_PATH}/{dir_name_}/{f_name_}",
+                data_type=Image(
+                    display_name=f_name_, mime_type=MIMEType.JPEG
+                ),
+                with_local_fs_data=False,
+            ), annotation))
+    ds.commit()
+    ds.close()
+
+
+if __name__ == "__main__":
+    do_iter_item()
---
+++ example/datasets/culane/example.py
@@ -0,0 +1,23 @@
+import io
+
+import numpy
+from PIL import Image as PILImage
+from PIL import ImageDraw, Image
+from starwhale import dataset, URI, URIType
+
+ds_name = "culane/version/latest"
+ds = dataset(ds_name)
+row = ds.fetch_one()
+data = row.data
+annotations = row.annotations
+with PILImage.open(io.BytesIO(data.fp)) as img, PILImage.open(
+    io.BytesIO(annotations["mask"].to_bytes(ds_name))
+).convert("RGBA") as msk:
+    draw = ImageDraw.Draw(img)
+    for line in annotations["lines"]:
+        draw.line([float(l) for l in line.split()], width=3, fill="red")
+    a = numpy.asarray(msk) * 50  # a is readonly
+    msk = Image.fromarray(a)
+    msk.putalpha(127)
+    img.paste(msk, (0, 0), mask=msk)
+    img.show()
