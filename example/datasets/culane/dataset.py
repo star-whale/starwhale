0fa0ef2e9db7e085c9f7085c9199516a1d12d28c
anda-ren <renyanda@starwhale.ai>
2022-12-19 15:19:16 +0800

    add timeout and retry for http request

--- example/datasets/culane/dataset.py
+++ example/datasets/culane/dataset.py
@@ -3,16 +3,21 @@ import json
 import requests
 
 from starwhale import Link, Image, dataset, MIMEType  # noqa: F401
+from starwhale.utils.retry import http_retry
 
 PATH_ROOT = "https://starwhale-examples.oss-cn-beijing.aliyuncs.com/dataset/culane"
 DATA_PATH = "driver_182_30frame"
 SEG_LABEL_PATH = f"laneseg_label_w16/{DATA_PATH}"
 
 
+@http_retry
+def request_link_text(anno_link):
+    return requests.get(anno_link, timeout=10).text
+
+
 def do_iter_item():
     ds = dataset("culane", create=True)
-    response = requests.get(f"{PATH_ROOT}/{DATA_PATH}/tree.json")
-    tree = json.loads(response.text)
+    tree = json.loads(request_link_text(f"{PATH_ROOT}/{DATA_PATH}/tree.json"))
     for dir in tree["children"]:
         dir_name_: str = dir["name"]
         if not dir_name_.endswith(".MP4") or dir["type"] != "directory":
@@ -23,17 +28,9 @@ def do_iter_item():
             if not f_name_.endswith(".jpg"):
                 continue
             name_no_suff = f_name_.split(".")[0]
-            splitlines = []
-            while True:
-                try:
-                    splitlines = requests.get(
-                        f"{PATH_ROOT}/{DATA_PATH}/{dir_name_}/{name_no_suff}.lines.txt"
-                    ).text.splitlines()
-                    break
-                except Exception:
-                    print(f"timeout {name_no_suff}")
-                    continue
-
+            splitlines = request_link_text(
+                f"{PATH_ROOT}/{DATA_PATH}/{dir_name_}/{name_no_suff}.lines.txt"
+            ).splitlines()
             annotation = {
                 "mask": Link(
                     with_local_fs_data=False,
