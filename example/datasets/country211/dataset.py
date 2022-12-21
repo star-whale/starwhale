fdc9eef83e6ea55de7b744ad8338f5580e67f84a
anda-ren <renyanda@starwhale.ai>
2022-12-21 14:35:15 +0800

    add country211 dataset

---
+++ example/datasets/country211/dataset.py
@@ -0,0 +1,50 @@
+import json
+
+import requests
+
+from starwhale import Link, Image, dataset, MIMEType  # noqa: F401
+from starwhale.utils.retry import http_retry
+
+PATH_ROOT = "https://starwhale-examples.oss-cn-beijing.aliyuncs.com/dataset/country211"
+DATA_PATH = "."
+
+
+@http_retry
+def request_link_text(anno_link):
+    return requests.get(anno_link, timeout=10).text
+
+
+def build_ds(split: str = "test"):
+    ds = dataset("country211", create=True)
+    tree = json.loads(request_link_text(f"{PATH_ROOT}/tree.json"))
+    for dir in tree:
+        if DATA_PATH not in dir.get("name", ""):
+            continue
+        for d in dir["contents"]:
+            if d["type"] != "directory":
+                continue
+            if d["name"] != split:
+                continue
+            for _d in d["contents"]:
+                category = _d["name"]
+                for f in _d["contents"]:
+                    _name = f["name"]
+                    ds.append(
+                        (
+                            f"{category}/{_name}",
+                            Link(
+                                uri=f"{PATH_ROOT}/{split}/{category}/{_name}",
+                                data_type=Image(
+                                    display_name=_name, mime_type=MIMEType.JPEG
+                                ),
+                                with_local_fs_data=False,
+                            ),
+                            {"label": category},
+                        )
+                    )
+    ds.commit()
+    ds.close()
+
+
+if __name__ == "__main__":
+    build_ds("test")
---
+++ example/datasets/country211/example.py
@@ -0,0 +1,16 @@
+import io
+
+from PIL import Image as PILImage
+from PIL import ImageDraw
+
+from starwhale import dataset
+
+ds_name = "country211/version/latest"
+ds = dataset(ds_name)
+row = ds.fetch_one()
+data = row.data
+annotations = row.annotations
+with PILImage.open(io.BytesIO(data.fp)) as img:
+    draw = ImageDraw.Draw(img)
+    draw.text((28, 36), annotations["label"], fill="red")
+    img.show()
