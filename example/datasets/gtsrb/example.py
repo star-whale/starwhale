b2f47be4608e5d36b64f6e6aee104405c6327ba9
renyanda <781905270@qq.com>
2022-12-23 11:48:21 +0800

    example(dataset): add GTSRB dataset (#1649)
    add GTSRB dataset

---
+++ example/datasets/gtsrb/example.py
@@ -0,0 +1,33 @@
+import io
+
+from PIL import Image as PILImage
+from PIL import ImageDraw
+
+from starwhale import dataset
+
+
+def draw_bbox(draw, bbox_view_):
+    x, y, w, h = bbox_view_.x, bbox_view_.y, bbox_view_.width, bbox_view_.height
+    draw.rectangle(
+        [
+            (x, y),
+            (
+                x + w,
+                y + h,
+            ),
+        ],
+        fill=None,
+        outline="red",
+    )
+
+
+ds_name = "gtsrb/version/latest"
+ds = dataset(ds_name)
+row = ds.fetch_one()
+data = row.data
+annotations = row.annotations
+with PILImage.open(io.BytesIO(data.fp)) as img:
+    draw = ImageDraw.Draw(img)
+    draw_bbox(draw, annotations["bbox"])
+    draw.text((0, 0), annotations["class"], fill="blue")
+    img.show()
