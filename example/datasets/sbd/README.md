ca28035604283766bb9d386f0c514cfff6ae7f1b
anda-ren <renyanda@starwhale.ai>
2022-12-26 20:12:14 +0800

    add sbd dataset

---
+++ example/datasets/sbd/README.md
@@ -0,0 +1,29 @@
+---
+title: The `caltech-101` Dataset
+---
+
+## The Caltech 101 Dataset Description
+
+- [Homepage](https://data.caltech.edu/records/mzrjq-6wc02)
+
+## The `caltech-101` dataset Structure
+
+### Data Fields
+
+- `data`: `starwhale.Image` loaded as bytes array
+- `annotations` of type dict:
+    - `label`: the label for the image
+
+## Build `caltech-101` Dataset locally
+
+```shell
+python3 dataset.py
+```
+
+## Example
+
+Output the first 1 record of the `caltech-101` dataset.
+
+```shell
+python3 example.py
+```
---
+++ example/datasets/sbd/example.py
@@ -0,0 +1,25 @@
+import io
+
+import numpy
+from PIL import Image as PILImage
+from PIL import ImageDraw
+
+from starwhale import dataset
+
+ds_name = "sbd/version/latest"
+ds = dataset(ds_name)
+row = ds["2008_000202"]
+data = row.data
+annotations = row.annotations
+with PILImage.open(io.BytesIO(data.fp)) as img:
+    draw = ImageDraw.Draw(img)
+
+    msk = PILImage.fromarray(
+        numpy.frombuffer(annotations["boundries"], dtype=numpy.uint8).reshape(
+            annotations["shape"]
+        )
+        * 50
+    ).convert("RGBA")
+    msk.putalpha(127)
+    img.paste(msk, (0, 0), mask=msk)
+    img.show()
