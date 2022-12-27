49c5fa362c62c295ba892f4b9682e5f7ef356db9
liuyunxi <205407@qq.com>
2022-12-27 21:11:47 +0800

    ut

--- client/tests/base/test_cloud.py
+++ client/tests/base/test_cloud.py
@@ -1,7 +1,13 @@
+from http import HTTPStatus
+
 import yaml
 from pyfakefs.fake_filesystem_unittest import TestCase
-
-from starwhale.base.cloud import CloudRequestMixed
+from requests_mock import Mocker
+from starwhale.base.cloud import CloudRequestMixed, CloudBundleModelMixin
+from starwhale.base.type import URIType
+from starwhale.base.uri import URI
+from starwhale.consts import HTTPMethod
+from starwhale.utils.config import SWCliConfigMixed
 
 
 class TestCloudRequestMixed(TestCase):
@@ -24,3 +30,30 @@ class TestCloudRequestMixed(TestCase):
         item = {"meta": "no dataset byte size"}
         size = ins.get_bundle_size_from_resp("dataset", item)
         assert size == 0
+
+    @Mocker()
+    def test_bundle_list(self, rm: Mocker) -> None:
+        sw = SWCliConfigMixed()
+        sw.update_instance(
+            uri="http://1.1.1.1", user_name="test", sw_token="123", alias="test"
+        )
+
+        rm.request(
+            HTTPMethod.GET,
+            "http://1.1.1.1/api/v1/project/sw/model?pageNum=1&pageSize=20",
+            json={
+                "data": {
+                    "list": [],
+                    "total": 10,
+                    "size": 10,
+                }
+            },
+            status_code=HTTPStatus.OK,
+        )
+
+        cbm = CloudBundleModelMixin()
+        _uri = URI("http://1.1.1.1/project/sw", expected_type=URIType.PROJECT)
+        _models, _pager = cbm._fetch_bundle_all_list(_uri,
+                                                     uri_typ=URIType.MODEL)
+
+        assert _models == {}
