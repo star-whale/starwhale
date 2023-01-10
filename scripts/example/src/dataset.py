2b004ee3473442cf92f3c9992f26c3c12e07f451
anda-ren <renyanda@starwhale.ai>
2023-01-10 15:14:37 +0800

    change simple test

--- scripts/example/src/dataset.py
+++ scripts/example/src/dataset.py
@@ -6,6 +6,4 @@ from starwhale import Text, BuildExecutor
 class SimpleTextDatasetBuildExecutor(BuildExecutor):
     def iter_item(self) -> t.Generator[t.Tuple[t.Any, t.Any], None, None]:
         for idx in range(0, 100):
-            data = Text(f"data-{idx}", encoding="utf-8")
-            annotations = {"label": f"label-{idx}"}
-            yield data, annotations
+            yield {"txt": Text(f"data-{idx}", encoding="utf-8"), "label": f"label-{idx}"}
