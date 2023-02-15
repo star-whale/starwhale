6e56f135578dab8a659753cdb0819ecf236ec8d5
gaoxinxing <15031259256@163.com>
2023-02-15 10:49:22 +0800

    optimise task status

--- server/controller/src/main/java/ai/starwhale/mlops/schedule/k8s/JobEventHandler.java
+++ server/controller/src/main/java/ai/starwhale/mlops/schedule/k8s/JobEventHandler.java
@@ -27,6 +27,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 @Slf4j
+@Deprecated
 @Component
 public class JobEventHandler implements ResourceEventHandler<V1Job> {
 
