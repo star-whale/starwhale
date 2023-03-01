f2a5eef9d59d71d5ab1985a0371df0a17f3e3c1d
gaoxinxing <15031259256@163.com>
2023-03-01 18:07:45 +0800

    fix realtime log gibberish

--- server/controller/src/main/java/ai/starwhale/mlops/domain/task/status/watchers/log/CancellableTaskLogK8sCollector.java
+++ server/controller/src/main/java/ai/starwhale/mlops/domain/task/status/watchers/log/CancellableTaskLogK8sCollector.java
@@ -22,7 +22,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-
 import okhttp3.Call;
 import okhttp3.Response;
 
