691fd98a9e2c3c347215d3dd2ed0394e95c2fa50
waynelwz <wytheme.go@gmail.com>
2023-03-14 18:08:19 +0800

    optimize: cache header deps to avoid rerender

--- console/packages/starwhale-ui/src/base/data-table/headers/header.tsx
+++ console/packages/starwhale-ui/src/base/data-table/headers/header.tsx
@@ -3,6 +3,7 @@ import HeaderCell from './header-cell'
 import type { ColumnT, DataTablePropsT, RowT, SortDirectionsT, RowActionT } from '../types'
 import { IStore } from '../store'
 import { themedUseStyletron } from '../../../theme/styletron'
+import _ from 'lodash'
 
 export const HEADER_ROW_HEIGHT = 44
 
@@ -121,7 +122,6 @@ function Header(props: HeaderProps) {
     function getPositionX(el) {
         const rect = el.getBoundingClientRect()
         return rect.left + window.scrollX
-        return 0
     }
 
     React.useLayoutEffect(() => {
@@ -159,13 +159,16 @@ function Header(props: HeaderProps) {
             setEndResizePos(0)
         }
 
+        const mousemove = _.throttle(handleMouseMove, 200)
+        const mouseup = _.throttle(handleMouseUp, 200)
+
         if (isResizingThisColumn) {
-            document.addEventListener('mousemove', handleMouseMove)
-            document.addEventListener('mouseup', handleMouseUp)
+            document.addEventListener('mousemove', mousemove)
+            document.addEventListener('mouseup', mouseup)
         }
         return () => {
-            document.removeEventListener('mousemove', handleMouseMove)
-            document.removeEventListener('mouseup', handleMouseUp)
+            document.removeEventListener('mousemove', mousemove)
+            document.removeEventListener('mouseup', mouseup)
         }
     }, [
         props,
