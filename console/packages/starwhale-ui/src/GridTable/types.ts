9760ef3f64bb94ee970b6c48de46bf5c245a9942
waynelwz <wytheme.go@gmail.com>
2023-03-17 11:36:19 +0800

    fix: lint error

--- console/packages/starwhale-ui/src/GridTable/types.ts
+++ console/packages/starwhale-ui/src/GridTable/types.ts
@@ -24,6 +24,7 @@ export interface ITableProps {
     columns: any[]
     storeRef?: React.MutableRefObject<ITableState | undefined>
     emptyMessage?: React.ReactNode
+    emptyColumnMessage?: React.ReactNode
 }
 
 export interface IPaginationProps {
