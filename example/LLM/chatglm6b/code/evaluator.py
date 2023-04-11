219db5ef019261df8e8e924d3f55e2a1f8c0afaa
anda-ren <renyanda@starwhale.ai>
2023-04-11 17:08:26 +0800

    fix lint

--- example/LLM/chatglm6b/code/evaluator.py
+++ example/LLM/chatglm6b/code/evaluator.py
@@ -1,14 +1,21 @@
 import sys
 from pathlib import Path
 
-from transformers import AutoTokenizer, AutoModel
+from transformers import AutoModel, AutoTokenizer
 
 from starwhale import evaluation
 
 ROOTDIR = Path(__file__).parent.parent
-tokenizer = AutoTokenizer.from_pretrained(str(ROOTDIR/"models"), trust_remote_code=True)
-model = AutoModel.from_pretrained(str(ROOTDIR/"models"), trust_remote_code=True).half().cuda()
+tokenizer = AutoTokenizer.from_pretrained(
+    str(ROOTDIR / "models"), trust_remote_code=True
+)
+model = (
+    AutoModel.from_pretrained(str(ROOTDIR / "models"), trust_remote_code=True)
+    .half()
+    .cuda()
+)
 model = model.eval()
+
 
 @evaluation.predict
 def ppl(data: dict, **kw):
@@ -17,6 +24,6 @@ def ppl(data: dict, **kw):
     print(f"dataset: {text}\n chatglm6b: {response} \n")
     return response
 
-if __name__ == '__main__':
-    ppl({"text":sys.argv[1]})
 
+if __name__ == "__main__":
+    ppl({"text": sys.argv[1]})
--- example/LLM/chatglm6b/download_model.py
+++ example/LLM/chatglm6b/download_model.py
@@ -1,5 +1,8 @@
-from transformers import AutoTokenizer, AutoModel
+from transformers import AutoModel, AutoTokenizer
+
 tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm-6b", trust_remote_code=True)
-model = AutoModel.from_pretrained("THUDM/chatglm-6b", trust_remote_code=True).half().cuda()
+model = (
+    AutoModel.from_pretrained("THUDM/chatglm-6b", trust_remote_code=True).half().cuda()
+)
 tokenizer.save_pretrained("./models")
 model.save_pretrained("./models")
