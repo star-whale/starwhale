8da27d42a2ed98638a4795a01c3719ac349587b0
gaoxinxing <15031259256@163.com>
2023-04-13 19:11:19 +0800

    rename

--- server/controller/src/main/java/ai/starwhale/mlops/api/SettingsApi.java
+++ server/controller/src/main/java/ai/starwhale/mlops/api/SettingsApi.java
@@ -27,7 +27,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@Tag(name = "Runtime")
+@Tag(name = "Settings")
 @Validated
 public interface SettingsApi {
 
