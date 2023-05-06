205a99a4b08de3ef5fff6b7c6f24a6007eee694d
waynelwz <wytheme.go@gmail.com>
2023-05-06 14:06:03 +0800

    fix: wdyr top-level await error when build

--- console/src/wdyr.ts
+++ console/src/wdyr.ts
@@ -1,18 +1,14 @@
 import * as React from 'react'
 
-async function init() {
-    const { default: wdyr } = await import('@welldone-software/why-did-you-render')
+if (import.meta.env.DEV) {
+    import('@welldone-software/why-did-you-render').then((m) => {
+        if (window.location.search.indexOf('why-render') === -1) return
 
-    if (window.location.search.indexOf('why-render') !== -1) {
-        wdyr(React, {
+        m.default(React, {
             include: [/.*/],
             exclude: [/^BrowserRouter/, /^Link/, /^Route/],
             trackHooks: true,
             trackAllPureComponents: true,
         })
-    }
-}
-
-if (import.meta.env.DEV) {
-    init()
+    })
 }
