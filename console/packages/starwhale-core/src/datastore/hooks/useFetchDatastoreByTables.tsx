42734df58779055b92034541d71c4331d12a58be
waynelwz <wytheme.go@gmail.com>
2023-05-18 18:27:28 +0800

    update: cache scan table columns

--- console/packages/starwhale-core/src/datastore/hooks/useFetchDatastoreByTables.tsx
+++ console/packages/starwhale-core/src/datastore/hooks/useFetchDatastoreByTables.tsx
@@ -1,15 +1,20 @@
 import { useScanDatastore } from './useFetchDatastore'
 import useDatastoreMixedSchema from './useDatastoreMixedSchema'
 import { ScanTableRequest } from '../schemas/datastore'
+import { useRef } from 'react'
 
 export function useFetchDatastoreByTables(recordQuery: ScanTableRequest) {
     const recordInfo = useScanDatastore(recordQuery, true)
     const { records, columnTypes } = useDatastoreMixedSchema(recordInfo?.data)
 
+    // cache columnTypes, especially when query changed, record fetch again, columnTypes will be reset
+    const columnTypesRef = useRef(columnTypes)
+    if (recordInfo.isSuccess) columnTypesRef.current = columnTypes
+
     return {
         recordInfo,
         records,
-        columnTypes,
+        columnTypes: !recordInfo.isSuccess ? columnTypesRef.current : columnTypes,
     }
 }
 
