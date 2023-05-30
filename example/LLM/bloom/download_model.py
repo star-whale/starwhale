6a07e669ee4dcd0489cbcaa3919de7ae542619be
anda_ren <renyanda@starwhale.ai>
2023-05-30 14:01:37 +0800

    add bloom mt0xxl 11B model ppl

---
+++ example/LLM/bloom/download_model.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+
+ROOTDIR = Path(__file__).parent
+checkpoint = "bigscience/mt0-xxl"
+
+tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+model = AutoModelForSeq2SeqLM.from_pretrained(
+    checkpoint, torch_dtype="auto", device_map="auto"
+)
+print(str(ROOTDIR / "models"))
+tokenizer.save_pretrained(str(ROOTDIR / "models"))
+model.save_pretrained(str(ROOTDIR / "models"))
+del model
+del tokenizer
