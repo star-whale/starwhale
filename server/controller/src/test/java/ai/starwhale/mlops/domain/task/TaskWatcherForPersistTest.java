5b739a130f32cfd107775bbec4082a7630e45ae5
Jialei <3217223+jialeicui@users.noreply.github.com>
2023-06-15 11:24:49 +0800

    fix(controller): start time may be empty if job failed directly (#2354)

--- server/controller/src/test/java/ai/starwhale/mlops/domain/task/TaskWatcherForPersistTest.java
+++ server/controller/src/test/java/ai/starwhale/mlops/domain/task/TaskWatcherForPersistTest.java
@@ -16,6 +16,7 @@
 
 package ai.starwhale.mlops.domain.task;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
@@ -89,9 +90,56 @@ public class TaskWatcherForPersistTest {
                 .build();
         task = new WatchableTask(task, List.of(taskWatcherForPersist), new TaskStatusMachine());
         task.updateStatus(TaskStatus.RUNNING);
-        verify(taskMapper, times(0)).updateTaskStartedTime(task.getId(), new Date());
-        verify(taskMapper, times(0)).updateTaskFinishedTime(task.getId(), new Date());
+        verify(taskMapper, times(0)).updateTaskStartedTime(any(), any());
+        verify(taskMapper, times(0)).updateTaskFinishedTime(any(), any());
         verify(taskMapper, times(0)).updateTaskStatus(List.of(task.getId()), task.getStatus());
     }
 
+    @Test
+    public void testFailedDirectlyWithTimeCaptured() {
+        TaskMapper taskMapper = mock(TaskMapper.class);
+        TaskWatcherForPersist taskWatcherForPersist = new TaskWatcherForPersist(new TaskStatusMachine(), taskMapper);
+        Task task = Task.builder()
+                .id(1L)
+                .uuid(UUID.randomUUID().toString())
+                .status(TaskStatus.PREPARING)
+                .step(Step.builder().job(Job.builder().jobRuntime(JobRuntime.builder()
+                        .build()).build()).build())
+                .startTime(7L)
+                .finishTime(8L)
+                .build();
+
+        var watchableTask = new WatchableTask(task, List.of(taskWatcherForPersist), new TaskStatusMachine());
+        watchableTask.updateStatus(TaskStatus.FAIL);
+        // update start time use the start time in task
+        verify(taskMapper, times(0)).updateTaskStartedTime(any(), any());
+        verify(taskMapper, times(1)).updateTaskStartedTimeIfNotSet(task.getId(), new Date(7L));
+        verify(taskMapper, times(1)).updateTaskFinishedTime(task.getId(), new Date(8L));
+        verify(taskMapper, times(0)).updateTaskFinishedTimeIfNotSet(any(), any());
+        verify(taskMapper, times(1)).updateTaskStatus(List.of(task.getId()), task.getStatus());
+
+    }
+
+    @Test
+    public void testFailedDirectlyWithoutTimeCaptured() {
+        TaskMapper taskMapper = mock(TaskMapper.class);
+        TaskWatcherForPersist taskWatcherForPersist = new TaskWatcherForPersist(new TaskStatusMachine(), taskMapper);
+        Task task = Task.builder()
+                .id(1L)
+                .uuid(UUID.randomUUID().toString())
+                .status(TaskStatus.PREPARING)
+                .step(Step.builder().job(Job.builder().jobRuntime(JobRuntime.builder()
+                        .build()).build()).build())
+                .startTime(null)
+                .finishTime(null)
+                .build();
+
+        var watchableTask = new WatchableTask(task, List.of(taskWatcherForPersist), new TaskStatusMachine());
+        watchableTask.updateStatus(TaskStatus.FAIL);
+        verify(taskMapper, times(0)).updateTaskStartedTime(any(), any());
+        verify(taskMapper, times(1)).updateTaskStartedTimeIfNotSet(eq(task.getId()), argThat(d -> d.getTime() > 0));
+        verify(taskMapper, times(0)).updateTaskFinishedTime(any(), any());
+        verify(taskMapper, times(1)).updateTaskFinishedTimeIfNotSet(eq(task.getId()), argThat(d -> d.getTime() > 0));
+        verify(taskMapper, times(1)).updateTaskStatus(List.of(task.getId()), task.getStatus());
+    }
 }
