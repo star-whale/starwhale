bbaceb1595745cc51592f0be593ca8833b40e382
waynelwz <wytheme.go@gmail.com>
2023-07-04 11:42:12 +0800

    fix(console): load widget init missing

--- console/packages/starwhale-core/src/widget/hooks/useWidget.tsx
+++ console/packages/starwhale-core/src/widget/hooks/useWidget.tsx
@@ -1,25 +1,12 @@
 import { useEffect, useState } from 'react'
-import DNDListWidget from '@starwhale/widgets/DNDListWidget'
-import SectionWidget from '@starwhale/widgets/SectionWidget'
-import PanelTableWidget from '@starwhale/widgets/PanelTableWidget'
-import PanelRocAucWidget from '@starwhale/widgets/PanelRocAucWidget'
-import PanelHeatmapWidget from '@starwhale/widgets/PanelConfusionMatrixWidget'
 import WidgetFactory from '../WidgetFactory'
 import WidgetPlugin from '../WidgetPlugin'
 
-const modules = [DNDListWidget, SectionWidget, PanelTableWidget, PanelRocAucWidget, PanelHeatmapWidget]
-
 export function useWidget(widgetType: string) {
-    const [widget, setWidget] = useState<WidgetPlugin | undefined>(WidgetFactory.widgetMap.get(widgetType))
+    const [widget, setWidget] = useState<WidgetPlugin | undefined>(WidgetFactory.getWidget(widgetType))
 
     useEffect(() => {
-        if (!WidgetFactory.widgetMap.has(widgetType)) {
-            modules.forEach((w: WidgetPlugin<any>) => {
-                if (w.getType() === widgetType) WidgetFactory.register(w.getType(), w)
-            })
-        }
-
-        const temp = WidgetFactory.widgetMap.get(widgetType)
+        const temp = WidgetFactory.getWidget(widgetType)
 
         if (temp && temp !== widget) {
             setWidget(temp)
