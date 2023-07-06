fd66113da8cf11204ec7cf02e37d11dea17ee8c9
waynelwz <wytheme.go@gmail.com>
2023-07-06 15:46:01 +0800

    update(console): job failreason support tooltip/copy, new copy & text component

---
+++ console/packages/starwhale-ui/src/Text/Text.tsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import { Tooltip } from '../Tooltip'
+import { IconCopy } from '../Copy'
+import { themedUseStyletron } from '../theme/styletron'
+
+export interface ITextProps {
+    children?: React.ReactNode
+    size?: 'small' | 'medium' | 'large' | 'xlarge'
+    style?: React.CSSProperties
+    tooltip?: React.ReactNode
+    content?: string
+    maxWidth?: string
+}
+
+const fontSizeMap: { [k in Exclude<ITextProps['size'], undefined>]: string } = {
+    small: '12px',
+    medium: '14px',
+    large: '16px',
+    xlarge: '20px',
+}
+
+function Text({ children, style, size = 'medium', tooltip = '', content = '', maxWidth = '200px' }: ITextProps) {
+    const [css] = themedUseStyletron()
+
+    let Component = (
+        <span
+            style={style}
+            className={css({
+                fontSize: fontSizeMap[size],
+                WebkitLineClamp: 1,
+                display: 'inline-block',
+                overflow: 'hidden',
+                textOverflow: 'ellipsis',
+                verticalAlign: 'middle',
+                maxWidth,
+            })}
+        >
+            {children ?? content}
+        </span>
+    )
+
+    if (tooltip) {
+        Component = (
+            <Tooltip content={tooltip} showArrow placement='top'>
+                {Component}
+            </Tooltip>
+        )
+    }
+
+    return (
+        <>
+            {Component}
+            {content && <IconCopy content={content} />}
+        </>
+    )
+}
+
+function MonoText({ children, style, ...props }: ITextProps) {
+    return (
+        <Text {...props} style={{ ...style, fontFamily: 'Roboto Mono' }}>
+            {children}
+        </Text>
+    )
+}
+
+export { MonoText }
+export default Text
