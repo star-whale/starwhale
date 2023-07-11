5301a2ba81343dcf294811977bff3ae14b246847
anda-ren <renyanda@starwhale.ai>
2023-07-11 10:18:30 +0800

    fix simple test error

--- scripts/example/runtime_conda_init.sh
+++ scripts/example/runtime_conda_init.sh
@@ -8,11 +8,12 @@ if [ -z "$1" ];then
     echo "client repo path is not passed as the first arg for this script"
     exit 1
 fi
-swcli_wheel="$1/dist/starwhale-0.0.0.dev0-py3-none-any.whl"
+
+swcli_wheel="$1/dist/starwhale-${PYPI_RELEASE_VERSION:=0.0.0.dev0}-py3-none-any.whl"
 echo $swcli_wheel
 if [ ! -f "$swcli_wheel" ];then
     python3 -m pip install -r "$1"/requirements-install.txt
     make -C "$1" build-wheel
 fi
 
-cp "$swcli_wheel" "$SCRIPT_DIR"
+cp "$swcli_wheel" "$SCRIPT_DIR"/starwhale-0.0.0.dev0-py3-none-any.whl
