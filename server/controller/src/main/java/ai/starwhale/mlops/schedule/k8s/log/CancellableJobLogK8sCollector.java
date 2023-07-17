8171d160b71a293a5a40a38b57824c2dc5eb40aa
gaoxinxing <15031259256@163.com>
2023-07-17 18:11:06 +0800

    optimise log collector

--- server/controller/src/main/java/ai/starwhale/mlops/schedule/k8s/log/CancellableJobLogK8sCollector.java
+++ server/controller/src/main/java/ai/starwhale/mlops/schedule/k8s/log/CancellableJobLogK8sCollector.java
@@ -54,6 +54,11 @@ public class CancellableJobLogK8sCollector implements CancellableJobLogCollector
                 bufferedReader = new BufferedReader(
                         new InputStreamReader(resp.body().byteStream(), StandardCharsets.UTF_8));
             } else {
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
                 return resp.message();
             }
         }
