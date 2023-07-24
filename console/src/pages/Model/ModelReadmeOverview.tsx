920cad486dc1a424628071f4defab17f4602258c
waynelwz <wytheme.go@gmail.com>
2023-07-24 16:24:08 +0800

    feat(console): support model readme preview

---
+++ console/src/pages/Model/ModelReadmeOverview.tsx
@@ -0,0 +1,24 @@
+import { useCurrentModelFile } from '@/domain/model/hooks/useCurrentModelFile'
+import { BusyPlaceholder } from '@starwhale/ui'
+import React, { Suspense } from 'react'
+
+const Markdown = React.lazy(() => import('@starwhale/ui/Markdown/Markdown'))
+
+export default function ModelReadmeOverview() {
+    const { content } = useCurrentModelFile('src/README.md')
+
+    return (
+        <div
+            className='markdown-body flex-column'
+            style={{
+                paddingTop: '20px',
+                minHeight: '500px',
+            }}
+        >
+            <Suspense fallback={<BusyPlaceholder />}>
+                <Markdown>{content}</Markdown>
+            </Suspense>
+            {!content && <BusyPlaceholder type='empty' />}
+        </div>
+    )
+}
