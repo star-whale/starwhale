e3e08750829a9b9158698ac84f024df19a95bf67
gaoxinxing <15031259256@163.com>
2023-08-14 18:50:31 +0800

    add white list

--- scripts/e2e_test/remove_test_env.sh
+++ scripts/e2e_test/remove_test_env.sh
@@ -4,6 +4,15 @@
 
 set -x
 
+white_list_array=($(grep white_list ~/.config/e2e.config | cut -d "=" -f2 | tr ',' ' '))
+
+for ns in "${white_list_array[@]}";do
+  if [ "$ns" == "$1" ];then
+    echo "$1 is in white list, skip cleanup"
+    exit 0
+  fi
+done
+
 helm uninstall "$1" -n "$1" || echo "helm uninstall ns error"
 kubectl delete ns "$1" || echo "kubectl delete ns error"
 ssh "$2" "sudo rm -rf /mnt/data/starwhale/$1"
