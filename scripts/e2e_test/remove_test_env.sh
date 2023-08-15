74bd995db1ae611e935704cc423b2417a853dd0b
gaoxinxing <15031259256@163.com>
2023-08-15 10:21:57 +0800

    add white list

--- scripts/e2e_test/remove_test_env.sh
+++ scripts/e2e_test/remove_test_env.sh
@@ -3,15 +3,18 @@
 # $2 the host for the pv path
 
 set -x
+if [ -f "$HOME"/.config/e2e.config ];then
+  white_list_array=($(grep white_list "$HOME"/.config/e2e.config | cut -d "=" -f2 | tr ',' ' '))
 
-white_list_array=($(grep white_list ~/.config/e2e.config | cut -d "=" -f2 | tr ',' ' '))
-
-for ns in "${white_list_array[@]}";do
-  if [ "$ns" == "$1" ];then
-    echo "$1 is in white list, skip cleanup"
-    exit 0
-  fi
-done
+  for ns in "${white_list_array[@]}";do
+    if [ "$ns" == "$1" ];then
+      echo "$1 is in white list, skip cleanup"
+      exit 0
+    fi
+  done
+else
+  echo "$HOME/.config/e2e.config does not exist, skip white list check."
+fi
 
 helm uninstall "$1" -n "$1" || echo "helm uninstall ns error"
 kubectl delete ns "$1" || echo "kubectl delete ns error"
