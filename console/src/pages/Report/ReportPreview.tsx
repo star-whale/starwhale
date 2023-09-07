3f702726ebc82af32626fe9a54d7bfa5b0dded26
waynelwz <wytheme.go@gmail.com>
2023-09-07 10:52:24 +0800

    update(console): mobile experience notice

--- console/src/pages/Report/ReportPreview.tsx
+++ console/src/pages/Report/ReportPreview.tsx
@@ -2,10 +2,12 @@ import { useFetchReportPreview } from '@/domain/report/hooks/useReport'
 import User from '@/domain/user/components/User'
 import React from 'react'
 import { formatTimestampDateTime } from '@/utils/datetime'
-import { IconFont } from '@starwhale/ui'
+import { ExtendButton, IconFont } from '@starwhale/ui'
 import TiptapEditor from '@starwhale/ui/TiptapEditor'
 import Avatar from '@/components/Avatar'
 import { useQueryArgs } from '@/hooks/useQueryArgs'
+import { useMedia } from 'react-use'
+import useTranslation from '@/hooks/useTranslation'
 
 export default function ReportPreview() {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -13,6 +15,10 @@ export default function ReportPreview() {
     const { rid } = query || {}
     const info = useFetchReportPreview(rid)
     const { data } = info
+    const [t] = useTranslation()
+    const [isNotice, setIsNotice] = React.useState(true)
+
+    const isWide = useMedia('(min-width: 480px)')
 
     return (
         <div className='flex flex-column flex-1'>
@@ -42,6 +48,14 @@ export default function ReportPreview() {
             <div className='mb-20px flex-1'>
                 <TiptapEditor id={rid} initialContent={data?.content} editable={false} />
             </div>
+            {!isWide && isNotice && (
+                <div className='flex fixed bottom-0 left-0 right-0 h-10 lh-10 px-4 bg-[rgba(2,16,43,0.60)] text-xs text-white justify-between items-center'>
+                    {t('report.mobile.notice')}
+                    <ExtendButton iconDisable as='link' onClick={() => setIsNotice(false)}>
+                        <IconFont type='close' kind='white' size={20} />
+                    </ExtendButton>
+                </div>
+            )}
         </div>
     )
 }
