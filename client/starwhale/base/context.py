9fd2cb2637149444670d022e5af6ea62b79a0c14
gaoxinxing <15031259256@163.com>
2023-09-27 10:08:52 +0800

    remove origin project property

--- client/starwhale/base/context.py
+++ client/starwhale/base/context.py
@@ -26,7 +26,6 @@ class Context:
         log_project: t.Optional[Project] = None,
     ):
         self.run_project = run_project or Project()
-        self.project = self.run_project.name
         self.log_project = log_project or self.run_project
         self.version = version
         self.step = step
