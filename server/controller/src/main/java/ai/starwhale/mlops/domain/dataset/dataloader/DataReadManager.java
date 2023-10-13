63e9bfb3f1f15d4d817811e3732f962c8c2c2544
gaoxinxing <15031259256@163.com>
2023-10-13 12:50:58 +0800

    use weakHashMap

--- server/controller/src/main/java/ai/starwhale/mlops/domain/dataset/dataloader/DataReadManager.java
+++ server/controller/src/main/java/ai/starwhale/mlops/domain/dataset/dataloader/DataReadManager.java
@@ -23,11 +23,11 @@ import ai.starwhale.mlops.domain.dataset.dataloader.bo.Session;
 import ai.starwhale.mlops.domain.dataset.dataloader.dao.DataReadLogDao;
 import ai.starwhale.mlops.domain.dataset.dataloader.dao.SessionDao;
 import com.google.common.collect.Iterables;
-import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.WeakHashMap;
 import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections4.CollectionUtils;
@@ -41,7 +41,7 @@ public class DataReadManager {
     private final SessionDao sessionDao;
     private final DataReadLogDao dataReadLogDao;
     private final DataIndexProvider dataIndexProvider;
-    private final Map<String, LinkedList<DataReadLog>> sessionCache = new HashMap<>();
+    private final Map<String, LinkedList<DataReadLog>> sessionCache = new WeakHashMap<>();
     private final Integer cacheSize;
 
     public DataReadManager(SessionDao sessionDao,
