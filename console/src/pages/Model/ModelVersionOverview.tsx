dd105dd68748da6e54ee803fcba50f05d661f582
waynelwz <wytheme.go@gmail.com>
2023-10-20 14:14:59 +0800

    update: add handler to model overview

--- console/src/pages/Model/ModelVersionOverview.tsx
+++ console/src/pages/Model/ModelVersionOverview.tsx
@@ -5,12 +5,13 @@ import { useModelVersion } from '@model/hooks/useModelVersion'
 import { MonoText } from '@/components/Text'
 import Alias from '@/components/Alias'
 import Shared from '@/components/Shared'
-import { Toggle } from '@starwhale/ui'
+import { Toggle, Text } from '@starwhale/ui'
 import { toaster } from 'baseui/toast'
 import { useParams } from 'react-router-dom'
 import { fetchModelVersion, updateModelVersionShared } from '@/domain/model/services/modelVersion'
 import { useProject } from '@project/hooks/useProject'
 import { getAliasStr } from '@base/utils/alias'
+import _ from 'lodash'
 
 export default function ModelVersionOverview() {
     const { projectId, modelId, modelVersionId } = useParams<{
@@ -22,6 +23,10 @@ export default function ModelVersionOverview() {
     const { project } = useProject()
 
     const [t] = useTranslation()
+
+    const stepSpecs = modelVersion?.versionInfo.stepSpecs
+    const stepSourceGroup = _.groupBy(stepSpecs, 'job_name')
+
     const items = [
         {
             label: t('sth name', [t('Model')]),
@@ -67,6 +72,82 @@ export default function ModelVersionOverview() {
             label: t('Created At'),
             value:
                 modelVersion?.versionInfo.createdTime && formatTimestampDateTime(modelVersion?.versionInfo.createdTime),
+        },
+        {
+            label: t('model.handler'),
+            value: (
+                <div className='py-10px'>
+                    {Object.entries(stepSourceGroup).map(([handler, stepSource]) => {
+                        return (
+                            <div key={handler}>
+                                <div>
+                                    <Text content={handler}>{handler}</Text>
+                                </div>
+                                {stepSource?.map((spec, i) => {
+                                    return (
+                                        <div key={[spec?.name, i].join('')}>
+                                            <div
+                                                style={{
+                                                    display: 'flex',
+                                                    minWidth: '280px',
+                                                    lineHeight: '1',
+                                                    alignItems: 'stretch',
+                                                    gap: '20px',
+                                                    marginBottom: '10px',
+                                                }}
+                                            >
+                                                <div
+                                                    style={{
+                                                        padding: '5px 20px',
+                                                        minWidth: '280px',
+                                                        background: '#EEF1F6',
+                                                        borderRadius: '4px',
+                                                    }}
+                                                >
+                                                    <span style={{ color: 'rgba(2,16,43,0.60)' }}>
+                                                        {t('Step')}:&nbsp;
+                                                    </span>
+                                                    <span>{spec?.name}</span>
+                                                    <div style={{ marginTop: '3px' }} />
+                                                    <span style={{ color: 'rgba(2,16,43,0.60)' }}>
+                                                        {t('Task Amount')}:&nbsp;
+                                                    </span>
+                                                    <span>{spec?.replicas}</span>
+                                                </div>
+                                                {spec.resources &&
+                                                    spec.resources?.length > 0 &&
+                                                    spec.resources?.map((resource, j) => (
+                                                        <div
+                                                            key={j}
+                                                            style={{
+                                                                padding: '5px 20px',
+                                                                borderRadius: '4px',
+                                                                border: '1px solid #E2E7F0',
+                                                                // display: 'flex',
+                                                                alignItems: 'center',
+                                                            }}
+                                                        >
+                                                            <span style={{ color: 'rgba(2,16,43,0.60)' }}>
+                                                                {t('Resource')}:&nbsp;
+                                                            </span>
+                                                            <span> {resource?.type}</span>
+                                                            <div style={{ marginTop: '3px' }} />
+                                                            <span style={{ color: 'rgba(2,16,43,0.60)' }}>
+                                                                {t('Resource Amount')}:&nbsp;
+                                                            </span>
+                                                            <span>{resource?.request}</span>
+                                                            <br />
+                                                        </div>
+                                                    ))}
+                                            </div>
+                                        </div>
+                                    )
+                                })}
+                            </div>
+                        )
+                    })}
+                </div>
+            ),
         },
     ].filter((item) => {
         return project?.privacy === 'PUBLIC' || item.label !== t('Shared')
