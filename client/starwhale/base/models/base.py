8813577dae6a8239b7273da19d6b502333dc2639
Jialei <jialeicui@126.com>
2023-10-26 18:53:56 +0800

    chore(client): auto generated model uses custom base model

--- client/starwhale/base/models/base.py
+++ client/starwhale/base/models/base.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import typing as t
 
 from pydantic import BaseModel
+from fastapi.encoders import jsonable_encoder
 
 
 class SwBaseModel(BaseModel):
@@ -12,7 +13,18 @@ class SwBaseModel(BaseModel):
         # smart_union=True is used to make sure that pydantic will not convert str to int automatically
         smart_union = True
 
-    ...
+    def to_dict(
+        self, by_alias: bool = True, exclude_none: bool = True
+    ) -> t.Dict[str, t.Any]:
+        """
+        Convert a pydantic model to a dict.
+        Args:
+            by_alias: use alias or not, default is True.
+                e.g. if you have a field named `user_name` in your model,
+                and you set `by_alias=True`, then the key in the dict will be `userName`, otherwise it will be `user_name`
+            exclude_none: exclude None value or not, default is True.
+        """
+        return jsonable_encoder(self.dict(by_alias=by_alias, exclude_none=exclude_none))  # type: ignore
 
 
 class ListFilter(SwBaseModel):
---
+++ client/tests/test_base.py
@@ -0,0 +1,71 @@
+import typing as t
+
+from pydantic import Field
+
+from starwhale.base.models.base import SwBaseModel
+
+
+def test_to_dict():
+    class Foo(SwBaseModel):
+        int_field: int
+        str_field: str
+        bool_field: bool
+        float_field: float = Field(0.0, alias="floatField")
+        list_field: t.List[int]
+        dict_field: t.Optional[t.Dict[str, int]] = Field(None, alias="dictField")
+        opt_field: t.Optional[str] = None
+
+    foo = Foo(
+        int_field=1,
+        str_field="foo",
+        bool_field=True,
+        float_field=1.0,
+        list_field=[1, 2, 3],
+        dict_field={"foo": 1, "bar": 2},
+    )
+    assert foo.to_dict() == {
+        "int_field": 1,
+        "str_field": "foo",
+        "bool_field": True,
+        "floatField": 1.0,
+        "list_field": [1, 2, 3],
+        "dictField": {"foo": 1, "bar": 2},
+    }
+
+    assert foo.to_dict(by_alias=False) == {
+        "int_field": 1,
+        "str_field": "foo",
+        "bool_field": True,
+        "float_field": 1.0,
+        "list_field": [1, 2, 3],
+        "dict_field": {"foo": 1, "bar": 2},
+    }
+
+    assert foo.to_dict(exclude_none=False) == {
+        "int_field": 1,
+        "str_field": "foo",
+        "bool_field": True,
+        "floatField": 1.0,
+        "list_field": [1, 2, 3],
+        "dictField": {"foo": 1, "bar": 2},
+        "opt_field": None,
+    }
+
+    # test smart union
+    data = {
+        "int_field": "1",
+        "str_field": 1,
+        "bool_field": "True",
+        "floatField": "1.0",
+        "list_field": ["1", "2", "3"],
+        "dictField": {"foo": "1", "bar": "2"},
+    }
+    foo = Foo(**data)
+    assert foo.to_dict() == {
+        "int_field": 1,
+        "str_field": "1",
+        "bool_field": True,
+        "floatField": 1.0,
+        "list_field": [1, 2, 3],
+        "dictField": {"foo": 1, "bar": 2},
+    }
