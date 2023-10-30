236e507b62f69141dd8b72104c76204d8f5b545c
gaoxinxing <15031259256@163.com>
2023-10-30 13:53:24 +0800

    gen model

--- client/starwhale/base/client/models/models.py
+++ client/starwhale/base/client/models/models.py
@@ -281,8 +281,10 @@ class DatasetTagRequest(SwBaseModel):
 class DataIndexDesc(SwBaseModel):
     start: Optional[str] = None
     start_type: Optional[str] = Field(None, alias='startType')
+    start_inclusive: Optional[bool] = Field(None, alias='startInclusive')
     end: Optional[str] = None
     end_type: Optional[str] = Field(None, alias='endType')
+    end_inclusive: Optional[bool] = Field(None, alias='endInclusive')
 
 
 class NullableResponseMessageDataIndexDesc(SwBaseModel):
