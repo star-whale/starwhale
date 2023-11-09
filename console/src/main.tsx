3eed287531470ea189f9fbb78c3da30528effc46
waynelwz <wytheme.go@gmail.com>
2023-11-09 17:40:43 +0800

    update(console): use swagger template api and schema as default api

--- console/src/main.tsx
+++ console/src/main.tsx
@@ -12,7 +12,7 @@ import { registerLocales } from './i18n/locales'
 import '@starwhale/ui/Markdown/markdown.css'
 import '@unocss/reset/tailwind.css'
 import 'virtual:uno.css'
-import { apiInit } from '@/api'
+import { api, apiInit } from '@/api'
 
 // eslint-disable-next-line
 // @ts-ignore
