f4302c6c02ca7e0d551a405fd311184797915494
anda-ren <renyanda@starwhale.ai>
2023-11-22 14:35:35 +0800

    fix multiple same env on console

--- server/controller/src/main/java/ai/starwhale/mlops/domain/ft/FineTuneAppService.java
+++ server/controller/src/main/java/ai/starwhale/mlops/domain/ft/FineTuneAppService.java
@@ -229,8 +229,17 @@ public class FineTuneAppService {
                 if (null == env) {
                     env = new ArrayList<>();
                 }
-                env.addAll(envSupplier.get());
-                s.setEnv(env);
+                Map<String, String> envMap = env.stream().collect(Collectors.toMap(Env::getName, Env::getValue));
+                //if ENV vars in user's step spec conflicts with controller's, use controller's ENV
+                envMap.putAll(
+                        envSupplier.get().stream().collect(Collectors.toMap(Env::getName, Env::getValue))
+                );
+                s.setEnv(
+                        envMap.entrySet()
+                                .stream()
+                                .map(entry -> new Env(entry.getKey(), entry.getValue()))
+                                .collect(Collectors.toList())
+                );
                 s.verifyStepSpecArgs();
             }
             stepSpecOverWrites = Constants.yamlMapper.writeValueAsString(steps);
