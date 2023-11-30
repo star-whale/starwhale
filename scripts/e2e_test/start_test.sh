04c35372efb0c8f3dd63450475c74d23eb5fe07d
anda-ren <renyanda@starwhale.ai>
2023-11-30 18:47:56 +0800

    fix client test failure doesn't populate to parent script

--- scripts/e2e_test/start_test.sh
+++ scripts/e2e_test/start_test.sh
@@ -325,7 +325,7 @@ client_test() {
   if ! in_github_action; then
     unset http_proxy
     unset https_proxy
-    bash scripts/client_test/cli_test.sh all
+    bash scripts/client_test/cli_test.sh all || exit 1
   else
     timeout 20m bash scripts/client_test/cli_test.sh simple || exit 1
   fi
