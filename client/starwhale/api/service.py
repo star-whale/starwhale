5614c0c9fb4109b4588fb725ac176ffe5941bc0f
gaoxinxing <15031259256@163.com>
2023-11-30 11:31:57 +0800

    fix

--- client/starwhale/api/service.py
+++ client/starwhale/api/service.py
@@ -1,9 +1,5 @@
 from starwhale.api._impl.service.service import api, Service, ServiceType
 from starwhale.api._impl.service.types.llm import LLMChat
+from starwhale.api._impl.service.types.text_to_img import TextToImage
 
-__all__ = [
-    "Service",
-    "api",
-    "ServiceType",
-    "LLMChat",
-]
+__all__ = ["Service", "api", "ServiceType", "LLMChat", "TextToImage"]
